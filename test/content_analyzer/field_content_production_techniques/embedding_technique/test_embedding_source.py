d80507c4b9c4625d74b520537299932450dc7398
Silleellie <antonio.silletti@gmail.com>
2021-02-04 18:50:45 +0100

    corretto e revisionato TestGensimDownloader

--- test/content_analyzer/field_content_production_techniques/embedding_technique/test_embedding_source.py
+++ test/content_analyzer/field_content_production_techniques/embedding_technique/test_embedding_source.py
@@ -7,31 +7,42 @@ from orange_cb_recsys.content_analyzer.field_content_production_techniques.embed
 
 class TestGensimDownloader(TestCase):
     def test_load(self):
-        self.skipTest("SLOW")
         source = GensimDownloader('glove-twitter-25')
+
+        # result is a matrix containing 2 rows, one for 'title', one for 'plot'
         result = source.load("title plot")
 
-        expected = np.ndarray(shape=(2, 25))
-        expected[0, :] = [8.50130022e-01, 4.52620000e-01, -7.05750007e-03,
-                          -8.77380013e-01, 4.24479991e-01, -8.36589992e-01,
-                          8.04159999e-01, 3.74080002e-01, 4.30849999e-01,
-                          -6.39360011e-01, 1.19390003e-01, 1.13419998e+00,
-                          -3.20650005e+00, 9.31460023e-01, 3.65420014e-01,
-                          -3.19309998e-03, 1.97899997e-01, -3.29540014e-01,
-                          2.96719998e-01, 4.88689989e-01, -1.37870002e+00,
-                          7.52340019e-01, 2.03339994e-01, -6.79979980e-01,
-                          -8.91939998e-01]
-        expected[1, :] = [7.26029992e-01, 1.46909997e-01, 1.05829999e-01,
-                          2.84680009e-01, 2.31950000e-01, -7.86419988e-01,
-                          1.33580005e+00, -8.31910014e-01, 4.39669997e-01,
-                          -3.01629990e-01, 2.93879986e-01, 4.53700006e-01,
-                          -2.18440008e+00, 2.45710000e-01, 3.21599990e-01,
-                          6.92149997e-01, 6.65279984e-01, 5.34259975e-01,
-                          3.30240000e-03, -4.91389990e-01, -2.80680005e-02,
-                          6.41950011e-01, -9.63369980e-02, -9.50479984e-01,
-                          -3.88559997e-01]
+        # Now let's convert those 2 vector in words and
+        # they should return 'title' and 'plot'.
+        words = []
+        for v in result:
+            # 'similar_by_vector()' returns a list with top n
+            # words similar to the vector given. I'm interested only in the most similar
+            # so n = 1
+            top_1 = source.model.similar_by_vector(v, 1)[0]
+            words.append(top_1)
 
-        self.assertTrue(np.allclose(result, expected))
+        # the expected shape of result is (2, 25):
+        # 2 for words and 25 due to the model 'glove-twitter-25'
+        expected_shape = (2, 25)
+        self.assertEqual(expected_shape, result.shape)
+
+        # words contains [(title, 1.0), (plot, 1.0000001192092896)]
+        # So I'm using indices to access the tuples values.
+        # 'first_like' contains how similar is 'first_word' to the vector given result[0]
+        first_word = (words[0])[0]
+        first_like = (words[0])[1]
+        second_word = (words[1])[0]
+        second_like = (words[1])[1]
+
+        # Obviously due to approximation the conversion won't return the
+        # exact word, but if the likelihood it's equal to 1 with an approx of 'delta'
+        # I'm assuming it's exactly that word
+        self.assertEqual(first_word, "title")
+        self.assertAlmostEqual(first_like, 1, delta=1e-6)
+        self.assertEqual(second_word, "plot")
+        self.assertAlmostEqual(second_like, 1, delta=1e-6)
+
 
 
 class TestWikipedia2VecDownloader(TestCase):
