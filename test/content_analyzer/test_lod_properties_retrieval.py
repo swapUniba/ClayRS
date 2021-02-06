007280cf44b4307ef1aab37c243c4f1da9e06b35
Silleellie <antonio.silletti@gmail.com>
2021-02-06 19:23:12 +0100

    abilitato, revisionato e documentato test DBPedia

--- test/content_analyzer/test_lod_properties_retrieval.py
+++ test/content_analyzer/test_lod_properties_retrieval.py
@@ -5,39 +5,39 @@ from orange_cb_recsys.content_analyzer.exogenous_properties_retrieval import DBP
 
 class TestDBPediaMappingTechnique(TestCase):
     def test_get_properties(self):
-        self.skipTest("need SPARQL")
-        raw_content = {"Title": "Jumanji", "Year": "1995", "Rated": "PG", "Released": "15 Dec 1995",
-                       "Runtime": "104 min",
-                       "Genre": "Adventure, Family, Fantasy", "Director": "Joe Johnston",
-                       "Writer": "Jonathan Hensleigh (screenplay by), Greg Taylor (screenplay by), "
-                                 "Jim Strain (screenplay by), Greg Taylor (screen story by), Jim Strain ("
-                                 "screen story by), Chris Van Allsburg (screen story by), "
-                                 "Chris Van Allsburg (based on the book by)",
-                       "Actors": "Robin Williams, Jonathan Hyde, Kirsten Dunst, Bradley Pierce",
-                       "Plot": "After being trapped in a jungle board game for 26 years, "
-                               "a Man-Child wins his release from the game. But, no sooner has he arrived that he "
-                               "is forced to play again, and this time sets the creatures of "
-                               "the jungle loose on the city. Now it is up to him to stop them.",
-                       "Language": "English, French", "Country": "USA", "Awards": "4 wins & 9 nominations.",
-                       "Poster": "https://m.media-amazon.com/images/M/MV5BZTk2ZmUwYmEtNTcwZS00YmMyLWFkYjMtNTRmZDA3YWExMjc2XkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
-                       "Ratings": [{"Source": "Internet Movie Database", "Value": "6.9/10"},
-                                   {"Source": "Rotten Tomatoes", "Value": "53%"}, {"Source": "Metacritic", "Value": "39/100"}],
-                       "Metascore": "39", "imdbRating": "6.9", "imdbVotes": "260,909", "imdbID": "tt0113497", "Type": "movie",
-                       "DVD": "25 Jan 2000", "BoxOffice": "N/A", "Production": "Sony Pictures Home Entertainment",
-                       "Website": "N/A", "Response": "True"}
+
+        raw_content = {"Title": "Jumanji", "Year": "1995", "Rated": "PG",
+                       "Released": "15 Dec 1995", "Budget_source": "6.5E7",
+                       "cinematography": "", "only_local": "", "wiki_id": "3700174"}
 
         mapp = DBPediaMappingTechnique('Film', 'EN', 'Title')
-        prop = mapp.get_properties('1', raw_content)
 
+        # Get properties THAT HAVE VALUE in DBPEDIA ONLY
+        prop = mapp.get_properties('1', raw_content)
+        self.assertEqual(prop.value["cinematography"], 'http://dbpedia.org/resource/Thomas_E._Ackerman')
+        self.assertNotIn("only_local", prop.value)
+
+        # Get all properties in DBPEDIA + Get all properties IN LOCAL SOURCE
         mapp.mode = 'all'
         prop = mapp.get_properties('1', raw_content)
+        self.assertEqual(prop.value["completion_date"], "")
+        self.assertEqual(prop.value["cinematography"], "http://dbpedia.org/resource/Thomas_E._Ackerman")
+        self.assertEqual(prop.value["only_local"], "")
+        self.assertEqual(prop.value["Title"], "Jumanji")
 
+        # Get all properties in DBPEDIA ONLY
         mapp.mode = 'all_retrieved'
         prop = mapp.get_properties('1', raw_content)
+        self.assertEqual(prop.value["completion_date"], "")
+        self.assertEqual(prop.value["cinematography"], "http://dbpedia.org/resource/Thomas_E._Ackerman")
+        self.assertNotIn("only_local", prop.value)
 
+        # Get all properties in LOCAL with value from DBPEDIA
         mapp.mode = 'original_retrieved'
         prop = mapp.get_properties('1', raw_content)
+        self.assertEqual(prop.value["cinematography"], 'http://dbpedia.org/resource/Thomas_E._Ackerman')
+        self.assertEqual(prop.value["only_local"], '')
 
-        mapp.label_field = 'Genre'
-        mapp.mode = 'only_retrieved_evaluated'
+        # additional_filter Query
+        mapp = DBPediaMappingTechnique('Film', 'EN', 'Title', additional_filters={'budget': 'Budget_source', 'wikiPageID': 'wiki_id'})
         prop = mapp.get_properties('1', raw_content)
