df283a244690869aaab60cf85bbe77eb0f700bcf
Silleellie <antonio.silletti@gmail.com>
2021-02-06 19:23:52 +0100

    creato e documentato test lettura da dataset locale

--- test/content_analyzer/test_lod_properties_retrieval.py
+++ test/content_analyzer/test_lod_properties_retrieval.py
@@ -1,6 +1,7 @@
 from unittest import TestCase
 
-from orange_cb_recsys.content_analyzer.exogenous_properties_retrieval import DBPediaMappingTechnique
+from orange_cb_recsys.content_analyzer.exogenous_properties_retrieval import DBPediaMappingTechnique, \
+    PropertiesFromDataset
 
 
 class TestDBPediaMappingTechnique(TestCase):
@@ -41,3 +42,47 @@ class TestDBPediaMappingTechnique(TestCase):
         # additional_filter Query
         mapp = DBPediaMappingTechnique('Film', 'EN', 'Title', additional_filters={'budget': 'Budget_source', 'wikiPageID': 'wiki_id'})
         prop = mapp.get_properties('1', raw_content)
+        self.assertEqual(prop.value["cinematography"], 'http://dbpedia.org/resource/Thomas_E._Ackerman')
+
+
+class TestPropertiesFromDataset(TestCase):
+    def test_get_properties(self):
+        raw_content = {"Title": "Jumanji", "Year": "1995", "Rated": "PG", "Released": "15 Dec 1995",
+                       "Runtime": ""}
+
+        # mode is 'only_retrieved_evaluated' so all item with a blank value
+        # will be discarded
+        expected_no_blank = {"Title": "Jumanji", "Year": "1995", "Rated": "PG", "Released": "15 Dec 1995"}
+        mapp = PropertiesFromDataset()
+        prop = mapp.get_properties('1', raw_content)
+        self.assertEqual(prop.value, expected_no_blank)
+
+        # mode is 'all' so all item are fine, also those with
+        # a blank value
+        expected_w_blank = {"Title": "Jumanji", "Year": "1995", "Rated": "PG", "Released": "15 Dec 1995",
+                            "Runtime": ""}
+        mapp.mode = 'all'
+        prop = mapp.get_properties('1', raw_content)
+        self.assertEqual(prop.value, expected_w_blank)
+
+        # field_name_list with 'title' is passed, and mode is 'only_retrieved_evaluated',
+        # so only the field 'title' will be retrieved, if it has a value
+        expected_title_only = {'Title': 'Jumanji'}
+        mapp = PropertiesFromDataset(field_name_list=['Title'])
+        prop = mapp.get_properties('1', raw_content)
+        self.assertEqual(prop.value, expected_title_only)
+
+        # field_name_list with a nonexistent field is passed,
+        # since the mode is 'only_retrieved_evaluated' nothing will be retrieved
+        expected_empty = {}
+        mapp = PropertiesFromDataset(field_name_list=['pppp'])
+        prop = mapp.get_properties('1', raw_content)
+        self.assertEqual(prop.value, expected_empty)
+
+        # field_name_list with a nonexistent field is passed,
+        # since the mode is 'all' everything is retrieved, also the nonexistent field.
+        # It will have a blank value
+        expected_nonexistant = {'pppp': ''}
+        mapp.mode = "all"
+        prop = mapp.get_properties('1', raw_content)
+        self.assertEqual(prop.value, expected_nonexistant)
