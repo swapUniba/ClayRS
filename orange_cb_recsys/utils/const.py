018c98d9ae1415f9a69184b3a1c49842d1575d5e
Silleellie <antonio.silletti@gmail.com>
2021-02-12 18:22:49 +0100

    wrap stderr per fix log mentre presente progress bar

--- orange_cb_recsys/utils/const.py
+++ orange_cb_recsys/utils/const.py
@@ -7,20 +7,14 @@ from orange_cb_recsys.utils.custom_logger import CustomFormatter
 home_path = str(Path.home())
 DEVELOPING = True
 
-# logging.basicConfig(level=logging.INFO)
-#
-# logger = logging.getLogger('logger')
-#
-# logger.setLevel(logging.INFO)
+progressbar.streams.wrap_stderr()
 
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
 
-# create console handler with a higher log level
 ch = logging.StreamHandler()
 ch.setLevel(logging.DEBUG)
 
 ch.setFormatter(CustomFormatter())
 
 logger.addHandler(ch)
-
