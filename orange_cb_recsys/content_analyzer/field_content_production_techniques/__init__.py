616e4a845bedcc83eff755dd45e234c7f6cbeee6
Elio <elio.musacchio28@gmail.com>
2021-03-20 23:47:37 +0100

    aggiunto whoosh a setup

--- orange_cb_recsys/content_analyzer/field_content_production_techniques/__init__.py
+++ orange_cb_recsys/content_analyzer/field_content_production_techniques/__init__.py
@@ -1,6 +1,6 @@
 from .entity_linking import BabelPyEntityLinking
 from .field_content_production_technique import EmbeddingTechnique
-from .tf_idf import LuceneTfIdf
+from .tf_idf import WhooshTfIdf
 from .embedding_technique import Centroid, GensimDownloader, Wikipedia2VecDownloader, BinaryFile
 from .field_content_production_technique import FieldContentProductionTechnique, CollectionBasedTechnique, \
     SingleContentTechnique, EmbeddingSource, CombiningTechnique, SearchIndexing
