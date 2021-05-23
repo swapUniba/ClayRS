c221e7de4afb21dc5ce9cbb49d7a1f1ee15a5734
Elio <elio.musacchio28@gmail.com>
2021-05-23 22:58:49 +0200

    Modificate field content production techniques

--- orange_cb_recsys/content_analyzer/field_content_production_techniques/__init__.py
+++ orange_cb_recsys/content_analyzer/field_content_production_techniques/__init__.py
@@ -3,5 +3,5 @@ from .field_content_production_technique import EmbeddingTechnique
 from .tf_idf import WhooshTfIdf
 from .embedding_technique import Centroid, GensimDownloader, Wikipedia2VecDownloader, BinaryFile
 from .field_content_production_technique import FieldContentProductionTechnique, CollectionBasedTechnique, \
-    SingleContentTechnique, EmbeddingSource, CombiningTechnique, SearchIndexing
+    SingleContentTechnique, EmbeddingSource, CombiningTechnique, OriginalData, DefaultTechnique
 from .synset_document_frequency import SynsetDocumentFrequency
