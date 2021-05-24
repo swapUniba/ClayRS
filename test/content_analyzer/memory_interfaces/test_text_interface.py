7d7d9654c5689cbad15e73c4ad889fb281e7a989
Elio <elio.musacchio28@gmail.com>
2021-05-24 19:18:25 +0200

    Aggiornati test text interface

--- test/content_analyzer/memory_interfaces/test_text_interface.py
+++ test/content_analyzer/memory_interfaces/test_text_interface.py
@@ -3,7 +3,7 @@ from unittest import TestCase
 from orange_cb_recsys.content_analyzer.memory_interfaces import KeywordIndex, SearchIndex
 
 
-class TestTextInterface(TestCase):
+class TestIndexInterface(TestCase):
     def test_keyword_serialize(self):
         index = KeywordIndex("/keyword")
         try:
@@ -34,21 +34,87 @@ class TestTextInterface(TestCase):
             index.delete()
 
     def test_init_writing(self):
-        index1 = SearchIndex("init_writing")
-        index2 = SearchIndex("init_writing")
+        index1 = SearchIndex("./init_writing")
+        index2 = SearchIndex("./init_writing")
+        index3 = SearchIndex("./init_writing")
 
         try:
             index1.init_writing()
             index1.new_content()
             index1.new_field("content_id", "0")
-            index1.new_field("init_writing", "test")
+            index1.new_field("init_writing", "test1")
             index1.serialize_content()
             index1.stop_writing()
 
-            index2.init_writing()
-            self.assertEqual(index2.get_field("init_writing", "0"), "test")
+            # test to check that init_writing with False argument doesn't replace the old index but opens it
+            index2.init_writing(False)
+            index2.new_content()
+            index2.new_field("content_id", "1")
+            index2.new_field("init_writing", "test2")
+            index2.serialize_content()
+            index2.stop_writing()
+            self.assertEqual(index2.get_field("init_writing", "0"), "test1")
+            self.assertEqual(index2.get_field("init_writing", "1"), "test2")
+
+            # test to check that init_writing with True argument replaces the old index
+            index3.init_writing(True)
+            index3.new_content()
+            index3.new_field("content_id", "0")
+            index3.new_field("init_writing", "test3")
+            index3.serialize_content()
+            index3.stop_writing()
+            self.assertEqual(index3.get_field("init_writing", "0"), "test3")
+            with self.assertRaises(IndexError):
+                index3.get_field("init_writing", "1")
         finally:
             index1.delete()
             index2.delete()
+            index3.delete()
 
+    def test_get_field(self):
+        index = KeywordIndex("/keyword")
+
+        try:
+            index.init_writing()
+            index.new_content()
+            index.new_field("content_id", "0")
+            index.new_field("test1", ["this", "is", "a", "test"])
+            index.new_field("test2", ["this", "is", "a", "test", "for", "the", "Text", "Interface"])
+            index.serialize_content()
+            index.stop_writing()
+
+            # test for using get_field with content's id
+            result = index.get_field("test1", "0")
+            self.assertIsInstance(result, list)
+            self.assertEqual(result, ["this", "is", "a", "test"])
+
+            # test for using get_field with content's position in the index
+            result = index.get_field("test1", 0)
+            self.assertIsInstance(result, list)
+            self.assertEqual(result, ["this", "is", "a", "test"])
+        finally:
+            index.delete()
+
+    def test_get_tfidf(self):
+        index = KeywordIndex("/keyword")
+        try:
+            index.init_writing()
+            index.new_content()
+            index.new_field("content_id", "0")
+            index.new_field("test1", ["this", "is", "a", "test"])
+            index.new_field("test2", ["this", "is", "a", "test", "for", "the", "Text", "Interface"])
+            index.serialize_content()
+            index.stop_writing()
+
+            # test for using get_tf_idf with content's id
+            result = index.get_tf_idf("test1", "0")
+            self.assertIsInstance(result, dict)
+            self.assertEqual(result["this"], 0.0)
+
+            # test for using get_td_idf with content's position in the index
+            result = index.get_tf_idf("test1", 0)
+            self.assertIsInstance(result, dict)
+            self.assertEqual(result["this"], 0.0)
+        finally:
+            index.delete()
 
