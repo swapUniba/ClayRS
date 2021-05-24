b46274c4ef9df95a3bb76de4042244cec0017bb7
Elio <elio.musacchio28@gmail.com>
2021-05-24 19:15:44 +0200

    Migliorata astrazione metodo produce content

--- orange_cb_recsys/content_analyzer/field_content_production_techniques/tf_idf.py
+++ orange_cb_recsys/content_analyzer/field_content_production_techniques/tf_idf.py
@@ -8,7 +8,6 @@ from orange_cb_recsys.content_analyzer.information_processor import InformationP
 from orange_cb_recsys.content_analyzer.memory_interfaces.text_interface import KeywordIndex
 from orange_cb_recsys.content_analyzer.raw_information_source import RawInformationSource
 from orange_cb_recsys.utils.check_tokenization import check_tokenized, check_not_tokenized
-from orange_cb_recsys.utils.id_merger import id_merger
 
 
 class SkLearnTfIdf(TfIdfTechnique):
@@ -20,16 +19,14 @@ class SkLearnTfIdf(TfIdfTechnique):
         self.__corpus = []
         self.__tfidf_matrix = None
         self.__feature_names = None
-        self.__matching = {}
 
-    def produce_single_repr(self, content_id: str) -> FeaturesBagField:
+    def produce_single_repr(self, content_position: int) -> FeaturesBagField:
         """
-        Retrieves the tf-idf values, for terms in document that match with content_id,
+        Retrieves the tf-idf values, for terms in document in the defined content_position,
         from the pre-computed word - document matrix.
         """
-        doc = self.__matching[content_id]
-        feature_index = self.__tfidf_matrix[doc, :].nonzero()[1]
-        tfidf_scores = zip(feature_index, [self.__tfidf_matrix[doc, x] for x in feature_index])
+        feature_index = self.__tfidf_matrix[content_position, :].nonzero()[1]
+        tfidf_scores = zip(feature_index, [self.__tfidf_matrix[content_position, x] for x in feature_index])
 
         features = {}
         for word, score in [(self.__feature_names[i], score) for (i, score) in tfidf_scores]:
@@ -37,21 +34,18 @@ class SkLearnTfIdf(TfIdfTechnique):
 
         return FeaturesBagField(features)
 
-    def dataset_refactor(self, information_source: RawInformationSource, id_field_names: list,
-                         field_name: str, preprocessor_list: List[InformationProcessor]):
+    def dataset_refactor(self, information_source: RawInformationSource, field_name: str,
+                         preprocessor_list: List[InformationProcessor]) -> int:
         """
         Creates a corpus structure, a list of string where each string is a document.
         Then calls TfIdfVectorizer on this collection, obtaining term-document tf-idf matrix, the corpus is then deleted
         """
         self.__corpus = []
-        self.__matching = {}
 
         for raw_content in information_source:
             processed_field_data = self.process_data(raw_content[field_name], preprocessor_list)
 
             processed_field_data = check_not_tokenized(processed_field_data)
-            content_id = id_merger(raw_content, id_field_names)
-            self.__matching[content_id] = len(self.__corpus)
             self.__corpus.append(processed_field_data)
 
         tf_vectorizer = TfidfVectorizer(sublinear_tf=True)
@@ -61,10 +55,11 @@ class SkLearnTfIdf(TfIdfTechnique):
 
         self.__feature_names = tf_vectorizer.get_feature_names()
 
+        return self.__tfidf_matrix.shape[0]
+
     def delete_refactored(self):
         del self.__tfidf_matrix
         del self.__feature_names
-        del self.__matching
 
     def __str__(self):
         return "SkLearnTfIdf"
@@ -83,31 +78,33 @@ class WhooshTfIdf(TfIdfTechnique):
         self.__index = KeywordIndex('./frequency-index')
         self.__field_name = None
 
-    def produce_single_repr(self, content_id: str) -> FeaturesBagField:
+    def produce_single_repr(self, content_position: int) -> FeaturesBagField:
         """
         Retrieves the tf-idf value directly from the index
         """
-        return FeaturesBagField(self.__index.get_tf_idf(self.__field_name, content_id))
+        return FeaturesBagField(self.__index.get_tf_idf(self.__field_name, content_position))
 
-    def dataset_refactor(self, information_source: RawInformationSource, id_field_names: list,
-                         field_name: str, preprocessor_list: List[InformationProcessor]):
+    def dataset_refactor(self, information_source: RawInformationSource, field_name: str,
+                         preprocessor_list: List[InformationProcessor]):
         """
         Saves the processed data in a index that will be used for frequency calculation
         """
         self.__field_name = field_name
         self.__index = KeywordIndex('./' + field_name)
-        self.__index.init_writing()
+        self.__index.init_writing(True)
+        dataset_len = 0
         for raw_content in information_source:
             self.__index.new_content()
-            content_id = id_merger(raw_content, id_field_names)
-            self.__index.new_field("content_id", content_id)
             processed_field_data = self.process_data(raw_content[field_name], preprocessor_list)
 
             processed_field_data = check_tokenized(processed_field_data)
             self.__index.new_field(field_name, processed_field_data)
             self.__index.serialize_content()
+            dataset_len += 1
 
         self.__index.stop_writing()
+
+        return dataset_len
 
     def delete_refactored(self):
         self.__index.delete()
