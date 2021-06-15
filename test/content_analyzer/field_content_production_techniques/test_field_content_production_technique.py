9bac444e5883a79a99e63843a84678c8ad73ad5c
Elio <elio.musacchio28@gmail.com>
2021-06-16 00:55:49 +0200

    Modificati test embedding technique

--- test/content_analyzer/field_content_production_techniques/test_field_content_production_technique.py
+++ test/content_analyzer/field_content_production_techniques/test_field_content_production_technique.py
@@ -1,41 +1,13 @@
 from unittest import TestCase
 import os
 
-from orange_cb_recsys.content_analyzer.content_representation.content import EmbeddingField, StringField
-from orange_cb_recsys.content_analyzer.field_content_production_techniques.embedding_technique.combining_technique import \
-    Centroid
-from orange_cb_recsys.content_analyzer.field_content_production_techniques.embedding_technique.embedding_source import \
-    GensimDownloader
+from orange_cb_recsys.content_analyzer.content_representation.content import StringField
 from orange_cb_recsys.content_analyzer.field_content_production_techniques.field_content_production_technique import \
-    OriginalData, DefaultTechnique, DocumentEmbeddingTechnique, WordEmbeddingTechnique, SentenceEmbeddingTechnique
+    OriginalData, DefaultTechnique
 from orange_cb_recsys.content_analyzer.raw_information_source import JSONFile
 
 THIS_DIR = os.path.dirname(os.path.abspath(__file__))
 file_path = os.path.join(THIS_DIR, "../../../datasets/movies_info_reduced.json")
-
-
-class TestEmbeddingTechnique(TestCase):
-    def test_produce_content(self):
-        technique = DocumentEmbeddingTechnique(GensimDownloader('glove-twitter-25'), Centroid())
-
-        embedding_list = technique.produce_content("Title", [], JSONFile(file_path))
-
-        self.assertEqual(len(embedding_list), 20)
-        self.assertIsInstance(embedding_list[0], EmbeddingField)
-
-        technique = WordEmbeddingTechnique(GensimDownloader('glove-twitter-25'), Centroid())
-
-        embedding_list = technique.produce_content("Title", [], JSONFile(file_path))
-
-        self.assertEqual(len(embedding_list), 20)
-        self.assertIsInstance(embedding_list[0], EmbeddingField)
-
-        technique = SentenceEmbeddingTechnique(GensimDownloader('glove-twitter-25'), Centroid())
-
-        embedding_list = technique.produce_content("Title", [], JSONFile(file_path))
-
-        self.assertEqual(len(embedding_list), 20)
-        self.assertIsInstance(embedding_list[0], EmbeddingField)
 
 
 class TestOriginalData(TestCase):
