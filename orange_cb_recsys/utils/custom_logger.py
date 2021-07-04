d2c81df439556d2d02617366404fb82664cbd28d
Silleellie <antonio.silletti@gmail.com>
2021-07-04 21:57:42 +0200

    fix color missing logger

--- orange_cb_recsys/utils/custom_logger.py
+++ orange_cb_recsys/utils/custom_logger.py
@@ -1,39 +1,46 @@
 import logging
+from typing import Optional, Dict
+
+from colorama import Fore, Back, Style
 
 
-class CustomFormatter(logging.Formatter):
-    """Logging Formatter to add colors and count warning / errors"""
-    def __init__(self, format: str):
-        super().__init__()
+class ColoredFormatter(logging.Formatter):
+    """Colored log formatter."""
 
-        grey = "\x1b[38;21m"
-        yellow = "\u001b[33m"
-        red = "\u001b[31m"
-        bold_red = "\u001b[1m\u001b[31m"
-        reset = "\u001b[0m"
-        header = "\u001b[32m"
+    def __init__(self, *args, colors: Optional[Dict[str, str]] = None, **kwargs) -> None:
+        """Initialize the formatter with specified format strings."""
 
-        self.FORMATS = {
-            logging.DEBUG: grey + format + reset,
-            logging.INFO: reset + format + reset,
-            logging.WARNING: yellow + format + reset,
-            logging.ERROR: red + format + reset,
-            logging.CRITICAL: bold_red + format + reset
-        }
+        super().__init__(*args, **kwargs)
 
-    def format(self, record):
-        log_fmt = self.FORMATS.get(record.levelno)
-        formatter = logging.Formatter(log_fmt)
-        return formatter.format(record)
+        self.colors = colors if colors else {}
+
+    def format(self, record) -> str:
+        """Format the specified record as text."""
+
+        record.color = self.colors.get(record.levelname, '')
+        record.reset = Style.RESET_ALL
+
+        return super().format(record)
+
+
+formatter = ColoredFormatter(
+    "\r{color}{levelname}{reset} - {message} ({filename}:{lineno})",
+    style='{',
+    colors={
+        'DEBUG': Fore.CYAN,
+        'INFO': Fore.RESET,
+        'WARNING': Fore.YELLOW,
+        'ERROR': Fore.RED,
+        'CRITICAL': Fore.BLACK + Back.RED + Style.BRIGHT,
+    }
+)
 
 
 def getCustomLogger(name: str):
-    logger = logging.getLogger(name)
-    logger.setLevel(logging.INFO)
-
     handler = logging.StreamHandler()
-    handler.setLevel(logging.INFO)
-    handler.setFormatter(CustomFormatter("\r%(levelname)s - %(message)s (%(filename)s:%(lineno)d)"))
+    handler.setFormatter(formatter)
 
+    logger = logging.getLogger(name)
     logger.addHandler(handler)
+    logger.setLevel(logging.INFO)
     return logger
