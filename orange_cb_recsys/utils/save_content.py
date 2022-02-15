2c03b05e2a51106d0fe42363dca891bafc785c3c
silleellie <antonio.silletti@gmail.com>
2022-02-15 23:21:05 +0100

    refactored test after custom ratings object

--- orange_cb_recsys/utils/save_content.py
+++ orange_cb_recsys/utils/save_content.py
@@ -2,6 +2,19 @@ import os
 
 
 def get_valid_filename(output_directory: str, filename: str, format: str, overwrite: bool):
+    """
+    Method which gets a valid *filename.format* based on the overwrite parameter
+
+    If overwrite=False and there are existent file named as *filename.format* in the output directory, the method
+    checks if there are also files named as *filename (1).format*, *filename (2).format*, *filename (3).format*.
+    It stops at the first non existent *filename (x).format* in the output directory specified in the constructor.
+    Args:
+        filename (str): Name of the file to save
+        format (str): Format of the file to save
+
+    Returns:
+        A valid 'filename.format' string based on the overwrite parameter
+    """
     filename_try = "{}.{}".format(filename, format)
 
     if overwrite is False:
--- test/recsys/content_based_algorithm/index_query/test_index_query.py
+++ test/recsys/content_based_algorithm/index_query/test_index_query.py
@@ -1,9 +1,12 @@
 import os
+import unittest
 from unittest import TestCase
 import pandas as pd
 
+from orange_cb_recsys.content_analyzer import Ratings
 from orange_cb_recsys.recsys.content_based_algorithm.contents_loader import LoadedContentsIndex
-from orange_cb_recsys.recsys.content_based_algorithm.exceptions import NotPredictionAlg
+from orange_cb_recsys.recsys.content_based_algorithm.exceptions import NotPredictionAlg, OnlyNegativeItems, \
+    NoRatedItems, EmptyUserRatings
 from orange_cb_recsys.recsys.content_based_algorithm.index_query.index_query import IndexQuery
 from test import dir_test_files
 
@@ -11,7 +14,7 @@ from test import dir_test_files
 class TestIndexQuery(TestCase):
 
     def setUp(self) -> None:
-        self.ratings = pd.DataFrame.from_records([
+        ratings = pd.DataFrame.from_records([
             ("A000", "tt0114576", 1, "54654675"),
             ("A000", "tt0112453", -0.2, "54654675"),
             ("A001", "tt0114576", 0.8, "54654675"),
@@ -20,7 +23,8 @@ class TestIndexQuery(TestCase):
             ("A002", "tt0112453", -0.2, "54654675"),
             ("A002", "tt0113497", 0.5, "54654675"),
             ("A003", "tt0112453", -0.8, "54654675")],
-            columns=["from_id", "to_id", "score", "timestamp"])
+            columns=["user_id", "to_id", "score", "timestamp"])
+        self.ratings = Ratings.from_dataframe(ratings)
 
         self.filter_list = ['tt0112641', 'tt0112760', 'tt0112896']
 
@@ -29,47 +33,44 @@ class TestIndexQuery(TestCase):
         self.available_loaded_items = LoadedContentsIndex(index_path)
 
     def test_predict(self):
-
         alg = IndexQuery({'Plot': 'index_original'}, threshold=0)
-        user_ratings = self.ratings.query('from_id == "A000"')
+        user_ratings = self.ratings.get_user_interactions("A000")
 
         alg.process_rated(user_ratings, self.available_loaded_items)
         alg.fit()
 
-        already_seen_items = list(user_ratings['to_id'])
         # Will raise Exception since it's not a Score Prediction Algorithm
         with self.assertRaises(NotPredictionAlg):
-            alg.predict(already_seen_items, self.available_loaded_items)
+            alg.predict(user_ratings, self.available_loaded_items)
 
     def test_rank_single_representation(self):
         # Test single representation
         alg = IndexQuery({'Plot': 'index_original'}, threshold=0)
-        user_ratings = self.ratings.query('from_id == "A000"')
+        user_ratings = self.ratings.get_user_interactions("A000")
 
         alg.process_rated(user_ratings, self.available_loaded_items)
         alg.fit()
 
-        already_seen_items = list(user_ratings['to_id'])
         # rank with filter_list
-        res_filtered = alg.rank(already_seen_items, self.available_loaded_items, filter_list=self.filter_list)
-        item_ranked_set = set(res_filtered['to_id'])
+        res_filtered = alg.rank(user_ratings, self.available_loaded_items, filter_list=self.filter_list)
+        item_ranked_set = set([interaction_predicted.item_id for interaction_predicted in res_filtered])
         self.assertEqual(len(item_ranked_set), len(self.filter_list))
         self.assertCountEqual(item_ranked_set, self.filter_list)
 
         # rank without filter_list
-        res_all_unrated = alg.rank(already_seen_items, self.available_loaded_items)
-        item_rated_set = set(user_ratings['to_id'])
-        item_ranked_set = set(res_all_unrated['to_id'])
+        res_all_unrated = alg.rank(user_ratings, self.available_loaded_items)
+        item_rated_set = set([interaction.item_id for interaction in user_ratings])
+        item_ranked_set = set([interaction_predicted.item_id for interaction_predicted in res_all_unrated])
         # We expect this to be empty, since the alg should rank only unrated items (unless in filter list)
         rated_in_ranked = item_ranked_set.intersection(item_rated_set)
         self.assertEqual(len(rated_in_ranked), 0)
 
         # rank with n_recs specified
         n_recs = 5
-        res_n_recs = alg.rank(already_seen_items, self.available_loaded_items, n_recs)
+        res_n_recs = alg.rank(user_ratings, self.available_loaded_items, n_recs)
         self.assertEqual(len(res_n_recs), n_recs)
-        item_rated_set = set(user_ratings['to_id'])
-        item_ranked_set = set(res_n_recs['to_id'])
+        item_rated_set = set([interaction.item_id for interaction in user_ratings])
+        item_ranked_set = set([interaction_predicted.item_id for interaction_predicted in res_n_recs])
         # We expect this to be empty, since the alg should rank only unrated items (unless in filter list)
         rated_in_ranked = item_ranked_set.intersection(item_rated_set)
         self.assertEqual(len(rated_in_ranked), 0)
@@ -78,32 +79,71 @@ class TestIndexQuery(TestCase):
         # Multiple representations with auto threshold based on the mean ratings of the user
         alg = IndexQuery({'Plot': ['index_original', 'index_preprocessed'],
                           'Genre': ['index_original', 3]})
-        user_ratings = self.ratings.query('from_id == "A000"')
+        user_ratings = self.ratings.get_user_interactions("A000")
 
         alg.process_rated(user_ratings, self.available_loaded_items)
         alg.fit()
 
-        already_seen_items = list(user_ratings['to_id'])
         # rank with filter_list
-        res_filtered = alg.rank(already_seen_items, self.available_loaded_items, filter_list=self.filter_list)
-        item_ranked_set = set(res_filtered['to_id'])
+        res_filtered = alg.rank(user_ratings, self.available_loaded_items, filter_list=self.filter_list)
+        item_ranked_set = set([interaction_predicted.item_id for interaction_predicted in res_filtered])
         self.assertEqual(len(item_ranked_set), len(self.filter_list))
         self.assertCountEqual(item_ranked_set, self.filter_list)
 
         # rank without filter_list
-        res_all_unrated = alg.rank(already_seen_items, self.available_loaded_items)
-        item_rated_set = set(user_ratings['to_id'])
-        item_ranked_set = set(res_all_unrated['to_id'])
+        res_all_unrated = alg.rank(user_ratings, self.available_loaded_items)
+        item_rated_set = set([interaction.item_id for interaction in user_ratings])
+        item_ranked_set = set([interaction_predicted.item_id for interaction_predicted in res_all_unrated])
         # We expect this to be empty, since the alg should rank only unrated items (unless in filter list)
         rated_in_ranked = item_ranked_set.intersection(item_rated_set)
         self.assertEqual(len(rated_in_ranked), 0)
 
         # rank with n_recs specified
         n_recs = 5
-        res_n_recs = alg.rank(already_seen_items, self.available_loaded_items, n_recs)
+        res_n_recs = alg.rank(user_ratings, self.available_loaded_items, n_recs)
         self.assertEqual(len(res_n_recs), n_recs)
-        item_rated_set = set(user_ratings['to_id'])
-        item_ranked_set = set(res_n_recs['to_id'])
+        item_rated_set = set([interaction.item_id for interaction in user_ratings])
+        item_ranked_set = set([interaction_predicted.item_id for interaction_predicted in res_n_recs])
         # We expect this to be empty, since the alg should rank only unrated items (unless in filter list)
         rated_in_ranked = item_ranked_set.intersection(item_rated_set)
         self.assertEqual(len(rated_in_ranked), 0)
+
+    def test_raise_errors(self):
+        # Only negative available
+        ratings = pd.DataFrame.from_records([
+            ("A000", "tt0112281", -1, "54654675")],
+            columns=["user_id", "to_id", "score", "timestamp"])
+        ratings = Ratings.from_dataframe(ratings)
+
+        alg = IndexQuery({'Plot': 'index_original'}, threshold=0)
+        user_ratings = ratings.get_user_interactions('A000')
+
+        with self.assertRaises(OnlyNegativeItems):
+            alg.process_rated(user_ratings, self.available_loaded_items)
+
+        # No Item available locally
+        ratings = pd.DataFrame.from_records([
+            ("A000", "non existent", 1, "54654675")],
+            columns=["user_id", "to_id", "score", "timestamp"])
+        ratings = Ratings.from_dataframe(ratings)
+
+        alg = IndexQuery({'Plot': 'index_original'}, threshold=0)
+        user_ratings = ratings.get_user_interactions('A000')
+
+        with self.assertRaises(OnlyNegativeItems):
+            alg.process_rated(user_ratings, self.available_loaded_items)
+
+        # User has no ratings
+        user_ratings = []
+
+        alg = IndexQuery({'Plot': 'index_original'}, threshold=0)
+
+        with self.assertRaises(EmptyUserRatings):
+            alg.process_rated(user_ratings, self.available_loaded_items)
+
+        with self.assertRaises(EmptyUserRatings):
+            alg.rank(user_ratings, self.available_loaded_items)
+
+
+if __name__ == '__main__':
+    unittest.main()
