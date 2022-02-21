af063c3ed997e17e5aebe573907c8ec281484c47
Giuseppe Sancesario <giuseppesancesario1999@outlook.com>
2022-02-21 13:15:30 +0100

    Refactor Ekphrasis

--- orange_cb_recsys/content_analyzer/information_processor/ekphrasis.py
+++ orange_cb_recsys/content_analyzer/information_processor/ekphrasis.py
@@ -1,9 +1,9 @@
 from orange_cb_recsys.content_analyzer.information_processor.information_processor import NLP
 
 import string
-from typing import List
+from typing import List, Dict
 
-from ekphrasis.classes.tokenizer import SocialTokenizer
+from ekphrasis.classes.tokenizer import SocialTokenizer, Tokenizer
 from ekphrasis.dicts.emoticons import emoticons
 import re
 from ekphrasis.classes.segmenter import Segmenter
@@ -16,133 +16,82 @@ class Ekphrasis(NLP):
     """
     Class to manage text to locate dates, currencies, etc.,
     unpack hashtags and correct spelling.
-
     """
 
     def __init__(self,
-                 spell_check: bool = False,
-                 unpack_hashtag: bool = False,
-                 convert_words: List = None,
-                 annotate_words: List = None,
+                 omit: List = None,
+                 normalize: List = None,
                  unpack_contractions: bool = False,
-                 annotate_emoji: bool = False,
-                 word_segmenter: bool = False,
-                 remove_punctuation: bool = False,
+                 unpack_hashtags: bool = False,
+                 annotate: List = None,
+                 corrector: str = None,
+                 tokenizer: Tokenizer = SocialTokenizer(lowercase=True).tokenize,
+                 segmenter: str = None,
+                 all_caps_tag: str = None,
+                 spell_correct_elong: bool = False,
+                 fix_text: bool = False,
+                 additional_substitution: List[Dict] = None
                  ):
-        if convert_words is None:
-            convert_words = []
 
-        if annotate_words is None:
-            annotate_words = []
+        self.text_processor = TextPreProcessor(tokenizer=tokenizer,
+                                               corrector=corrector, omit=omit,
+                                               normalize=normalize, segmenter=segmenter,
+                                               unpack_hashtags=unpack_hashtags,
+                                               annotate=annotate,
+                                               all_caps_tag=all_caps_tag, unpack_contractions=unpack_contractions,
+                                               spell_correct_elong=spell_correct_elong,
+                                               fix_text=fix_text,
+                                               dicts=additional_substitution
+                                               )
+        if corrector is not None:
+            self.spell_check = SpellCorrector(corpus=corrector)
 
-        self.text_processor = TextPreProcessor(tokenizer=SocialTokenizer(lowercase=False).tokenize,
-                                               normalize=convert_words,
-                                               unpack_hashtags=unpack_hashtag, annotate=annotate_words,
-                                               unpack_contractions=unpack_contractions, dicts=self.__annotate_emoji())
+        if segmenter is not None:
+            self.word_segmenter = Segmenter(corpus=segmenter)
 
-        self.annotate_emoji = annotate_emoji
-        self.word_segmenter = word_segmenter
-        self.remove_punctuation = remove_punctuation
-        self.spell_check = spell_check
-
-    @staticmethod
-    def __spell_check(field_data):
+    def __spell_check(self, field_data):
         """
         Correct any spelling errors
         Args:
             field_data: text to correct
-
         Returns:
             field_data: correct text
-
         """
-        sp = SpellCorrector()
+
         correct_sentence = []
         for word in field_data:
             if word[0] != "<" and word[0] != "#":
                 if word in string.punctuation:
                     correct_sentence.append(word)
                 else:
-                    correct_sentence.append(sp.correct(word))
+                    correct_sentence.append(self.spell_check.correct(word))
             else:
                 correct_sentence.append(word)
         return correct_sentence
-
-    def __remove_punctuation(self, text) -> List[str]:
-        """
-        Punctuation removal
-        Args:
-            text(str) : text with punct
-        Returns:
-            text_without_punct (str): text without punct
-        """
-        if isinstance(text, List):
-            text = self.list_to_string(text)
-        text = re.sub(r"[^\w\d<>\s]+", '', text)
-        text = self.string_to_list(text)
-        return text
-
-    def __annotate_emoji(self):
-        """
-
-        Returns: Dict of emoticons
-
-        """
-        if self.annotate_emoji:
-            return [emoticons]
 
     def __word_segmenter(self, field_data) -> List[str]:
         """
         Split words together
         Args:
             field_data: Text to be processed
-
         Returns (List[str): Text with splitted words
-
         """
         word_seg_list = []
-        seg_eng = Segmenter(corpus="english")
-        for w in field_data:
-            word_seg_list.append((seg_eng.segment(w)))
-        word_seg_list = self.list_to_string(word_seg_list)
+        for word in field_data:
+            word_seg_list.append((self.word_segmenter.segment(word)))
         return word_seg_list
-
-    def __strip_multiple_whitespaces_operation(self, text: List[str]) -> str:
-        """
-        Remove multiple whitespaces and '<repeated> marker on input text
-
-        Args:
-            text (str):
-
-        Returns:
-            str: input text, multiple whitespaces removed
-        """
-        text = self.list_to_string(text)
-        text = re.sub('<repeated>', ' ', text)
-        text = re.sub(' +', ' ', text)
-        text = self.string_to_list(text)
-        return text
 
     def process(self, field_data) -> List[str]:
 
         """
-
         Args:
             field_data: content to be processed
-
         Returns:
             field_data (List<str>): list of str or dict in case of named entity recognition
-
         """
-
         field_data = self.text_processor.pre_process_doc(field_data)
-        if self.remove_punctuation:
-            field_data = self.__remove_punctuation(field_data)
         if self.spell_check:
-            field_data = self.__spell_check(field_data)
+            field_data=self.__spell_check(field_data)
         if self.word_segmenter:
             field_data = self.__word_segmenter(field_data)
-        if isinstance(field_data, str):
-            field_data = self.string_to_list(field_data)
-        field_data = self.__strip_multiple_whitespaces_operation(field_data)
         return field_data
