2f699cbc96b72ad4e18c923845ec79c0267138ee
Silleellie <antonio.silletti@gmail.com>
2022-02-25 11:47:29 +0100

    fixed error test

--- test/content_analyzer/embeddings/embedding_learner/test_fasttext.py
+++ test/content_analyzer/embeddings/embedding_learner/test_fasttext.py
@@ -7,13 +7,12 @@ from orange_cb_recsys.content_analyzer.information_processor.nlp import NLTK
 from orange_cb_recsys.content_analyzer.raw_information_source import JSONFile
 from test import dir_test_files
 
-THIS_DIR = os.path.dirname(os.path.abspath(__file__))
 file_path = os.path.join(dir_test_files, 'movies_info_reduced.json')
 
 
 class TestGensimFastText(TestCase):
     def test_fit(self):
-        model_path = os.path.join(THIS_DIR, "/model_test_FastText")
+        model_path = "./model_test_FastText"
         learner = GensimFastText(model_path, True)
         learner.fit(source=JSONFile(file_path), field_list=["Plot", "Genre"], preprocessor_list=[NLTK()])
         model_path += ".bin"
--- test/content_analyzer/embeddings/embedding_learner/test_random_indexing.py
+++ test/content_analyzer/embeddings/embedding_learner/test_random_indexing.py
@@ -8,13 +8,12 @@ from orange_cb_recsys.content_analyzer.information_processor.nlp import NLTK
 from orange_cb_recsys.content_analyzer.raw_information_source import JSONFile
 from test import dir_test_files
 
-THIS_DIR = os.path.dirname(os.path.abspath(__file__))
 file_path = os.path.join(dir_test_files, 'movies_info_reduced.json')
 
 
 class TestRandomIndexing(TestCase):
     def test_fit(self):
-        model_path = os.path.join(THIS_DIR, "/model_test_Ri")
+        model_path = "./model_test_Ri"
         learner = GensimRandomIndexing(model_path, True)
         learner.fit(source=JSONFile(file_path), field_list=["Plot", "Genre"], preprocessor_list=[NLTK()])
         model_path += ".model"
