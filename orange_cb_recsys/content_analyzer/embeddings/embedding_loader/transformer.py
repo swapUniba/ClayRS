03ea726dbe67f734f38936e175d812b4a897270d
Silleellie <antonio.silletti@gmail.com>
2022-04-07 21:05:20 +0200

    fixed abstraction transformer

--- orange_cb_recsys/content_analyzer/embeddings/embedding_loader/transformer.py
+++ orange_cb_recsys/content_analyzer/embeddings/embedding_loader/transformer.py
@@ -11,10 +11,10 @@ from orange_cb_recsys.content_analyzer.field_content_production_techniques.embed
 
 class Transformers(SentenceEmbeddingLoader):
     """
-    This class loads the embeddings using the Transformers from pretrained model.
+    This class loads the embeddings using the Transformers from pretrained _model.
 
     Args:
-        model_name (str): name of the embeddings model to download or path where the model is stored locally
+        model_name (str): name of the embeddings _model to download or path where the _model is stored locally
         vec_strategy (VectorStrategy): strategy with which to combine layers for each token
         pooling_strategy (CombiningTechnique): strategy with which to combine embeddings for each token
     """
@@ -22,40 +22,40 @@ class Transformers(SentenceEmbeddingLoader):
     def __init__(self, model_name: str = 'bert-base-uncased',
                  vec_strategy: VectorStrategy = CatStrategy(1),
                  pooling_strategy: CombiningTechnique = Centroid()):
-        self.model = None
-        self.tokenizer = None
-        self.name_model = model_name
-        self.vec_strategy = vec_strategy
-        self.last_interesting_layers = vec_strategy.last_interesting_layers
-        self.pooling_strategy = pooling_strategy
+        self._model = None
+        self._tokenizer = AutoTokenizer.from_pretrained(model_name)
+        self._name_model = model_name
+        self._vec_strategy = vec_strategy
+        self._last_interesting_layers = vec_strategy.last_interesting_layers
+        self._pooling_strategy = pooling_strategy
         super().__init__(model_name)
 
     def load_model(self):
-        # we disable logger info on the load of the model
+        # we disable logger info on the load of the _model
         original_verb = transformers.logging.get_verbosity()
         transformers.logging.set_verbosity_error()
         try:
-            self.model = AutoModel.from_pretrained(self.name_model, output_hidden_states=True)
-            self.tokenizer = AutoTokenizer.from_pretrained(self.name_model)
+            self._model = AutoModel.from_pretrained(self._name_model, output_hidden_states=True)
 
             transformers.logging.set_verbosity(original_verb)
-            return self.model
+            return self._model
         except (OSError, AttributeError):
             raise FileNotFoundError
 
     def get_vector_size(self) -> int:
-        if isinstance(self.vec_strategy, CatStrategy):
-            return self.model.embeddings.token_type_embeddings.embedding_dim * self.last_interesting_layers
+        if isinstance(self._vec_strategy, CatStrategy):
+            return self.model.embeddings.token_type_embeddings.embedding_dim * self._last_interesting_layers
         else:
             return self.model.embeddings.token_type_embeddings.embedding_dim
 
     def get_embedding(self, sentence: str) -> np.ndarray:
         token_vecs = self.get_embedding_token(sentence)
-        sentence_vec = self.pooling_strategy.combine(token_vecs)
+        sentence_vec = self._pooling_strategy.combine(token_vecs)
         return sentence_vec
 
     def get_embedding_token(self, sentence: str) -> np.ndarray:
-        encoded = self.tokenizer.encode_plus(sentence)
+
+        encoded = self._tokenizer.encode_plus(sentence)
         tokens_tensor = torch.tensor([encoded['input_ids']])
         segments_tensors = torch.tensor([encoded['attention_mask']])
 
@@ -67,7 +67,7 @@ class Transformers(SentenceEmbeddingLoader):
         token_embeddings = torch.squeeze(token_embeddings, dim=1)
         token_embeddings = token_embeddings.permute(1, 0, 2)
 
-        token_vecs = self.vec_strategy.build_embedding(token_embeddings)
+        token_vecs = self._vec_strategy.build_embedding(token_embeddings)
 
         return token_vecs
 
@@ -81,11 +81,11 @@ class Transformers(SentenceEmbeddingLoader):
 
         """
         marked_text = "[CLS] " + sentence + " [SEP]"
-        tokenized_text = self.tokenizer.tokenize(marked_text)
+        tokenized_text = self.model.tokenize(marked_text)
         return tokenized_text
 
     def __str__(self):
-        return "Transformers: model = " + str(self.model)
+        return "Transformers: _model = " + str(self.model)
 
     def __repr__(self):
         return str(self)
