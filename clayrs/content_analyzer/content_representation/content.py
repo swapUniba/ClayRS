f6dcc818f2b2f9041eb5489a7182374a0b1ecf80
Silleellie <antonio.silletti@gmail.com>
2022-04-27 19:28:15 +0200

    slots for contents

--- clayrs/content_analyzer/content_representation/content.py
+++ clayrs/content_analyzer/content_representation/content.py
@@ -1,9 +1,7 @@
 from abc import ABC, abstractmethod
 from typing import Dict, Union, List, Tuple
 import numpy as np
-from _ctypes import PyObj_FromPtr
 import json
-import re
 
 from scipy import sparse
 
@@ -16,6 +14,7 @@ class FieldRepresentation(ABC):
     Abstract class that generalizes the concept of "field representation",
     a field representation is a semantic way to represent a field of an item.
     """
+    __slots__ = ()
 
     @abstractmethod
     def __str__(self):
@@ -46,6 +45,7 @@ class FeaturesBagField(FieldRepresentation):
     Args:
         features (dict<str, object>): the dictionary where features are stored
     """
+    __slots__ = ('__scores', '__pos_feature_tuples')
 
     def __init__(self, dense_scores: np.ndarray, pos_feature_tuples: List[Tuple[int, str]]):
         self.__scores: np.ndarray = dense_scores
@@ -84,6 +84,7 @@ class SimpleField(FieldRepresentation):
     Args:
         value (str): string representing the value of the field
     """
+    __slots__ = ('__value',)
 
     def __init__(self, value: object = None):
         self.__value: object = value
@@ -116,6 +117,7 @@ class EmbeddingField(FieldRepresentation):
         embedding_array (np.ndarray): embeddings array,
             it can be of different shapes according to the granularity of the technique
     """
+    __slots__ = ('__dense_array',)
 
     def __init__(self, embedding_array: np.ndarray):
         self.__dense_array = embedding_array
@@ -142,6 +144,7 @@ class IndexField(FieldRepresentation):
         index_id (int): position of the content in the index
         index (InformationInterface): index from which the data will be retrieved
     """
+    __slots__ = ('__field_name', '__index_id', '__index')
 
     def __init__(self, field_name: str, index_id: int, index: InformationInterface):
         self.__field_name = field_name
@@ -165,6 +168,7 @@ class ExogenousPropertiesRepresentation(ABC):
     """
     Output of LodPropertiesRetrieval, different representations exist according to different techniques
     """
+    __slots__ = ()
 
     @abstractmethod
     def __str__(self):
@@ -193,6 +197,7 @@ class PropertiesDict(ExogenousPropertiesRepresentation):
     Args:
         features: properties in the specified format
     """
+    __slots__ = ('__features',)
 
     def __init__(self, features: Dict[str, str] = None):
         if features is None:
@@ -218,6 +223,7 @@ class EntitiesProp(ExogenousPropertiesRepresentation):
     Args:
         features: properties in the specified format
     """
+    __slots__ = ('__features',)
 
     def __init__(self, features: Dict[str, Dict] = None):
         if features is None:
