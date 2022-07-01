59d8796765fdb36553b23b114f90879fe4834665
Silleellie <antonio.silletti@gmail.com>
2022-07-01 16:59:56 +0200

    updated setup with version and topics

--- setup.py
+++ setup.py
@@ -7,15 +7,35 @@ with open('requirements.txt') as f:
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+VERSION = "0.1.0"
+
 setup(name='clayrs',
-      version='0.3.9',
+      version=VERSION,
+      license='GPL-3.0',
       author='Antonio Silletti, Elio Musacchio, Roberta Sallustio',
       install_requires=requirements,
-      description='Python Framework for Content-Based Recommeder Systems',
+      description='Complexly represent contents, build recommender systems, evaluate them. All in one place!',
       long_description=long_description,
       long_description_content_type="text/markdown",
-      url='https://github.com/swapuniba/clayrs',
+      url='https://github.com/swapUniba/clayrs',
       include_package_data=True,
       packages=setuptools.find_packages(),
-      python_requires='>=3.7'
+      python_requires='>=3.7',
+
+      classifiers=[
+            'Development Status :: 3 - Alpha',
+            'Intended Audience :: Developers',
+            'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python',
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3 :: Only',
+            'Programming Language :: Python :: 3.7',
+            'Programming Language :: Python :: 3.8',
+            'Programming Language :: Python :: 3.9',
+            'Topic :: Software Development :: Libraries',
+            'Topic :: Software Development :: Libraries :: Python Modules',
+            'Topic :: Software Development :: Testing :: Unit'
+      ]
+
       )
