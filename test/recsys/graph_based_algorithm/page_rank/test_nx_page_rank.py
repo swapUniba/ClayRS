b621f4100dcb917f44b7e69ff1470bb33e7571ca
Silleellie <antonio.silletti@gmail.com>
2022-08-24 16:00:17 +0200

    updated test after small changes to gbrs algorithms

--- test/recsys/graph_based_algorithm/page_rank/test_nx_page_rank.py
+++ test/recsys/graph_based_algorithm/page_rank/test_nx_page_rank.py
@@ -20,6 +20,14 @@ ratings = pd.DataFrame.from_records([
     columns=["from_id", "to_id", "score", "timestamp"])
 ratings = Ratings.from_dataframe(ratings, timestamp_column="timestamp")
 
+test_set = pd.DataFrame.from_records([
+    ("A000", "tt0113497", 1, "54654675"),
+    ("A001", "tt0112453", 0.5, "54654675"),
+    ("A002", "tt0114576", 0.5, "54654675"),
+    ("A003", "tt0114576", 0, "54654675")],
+    columns=["from_id", "to_id", "score", "timestamp"])
+test_set = Ratings.from_dataframe(test_set, timestamp_column="timestamp")
+
 
 class TestNXPageRank(TestCase):
 
@@ -37,8 +45,8 @@ class TestNXPageRank(TestCase):
         # test not personalized
         alg = NXPageRank()
 
-        # rank without filter_list
-        res_all_unrated = alg.rank({'A000'}, self.graph)
+        # rank with None methodology (all unrated items will be ranked)
+        res_all_unrated = alg.rank({'A000'}, self.graph, test_set, methodology=None)
         item_rated_set = set([interaction.item_id for interaction in ratings.get_user_interactions("A000")])
         item_ranked_set = set([pred_interaction.item_id for pred_interaction in res_all_unrated])
         # We expect this to be empty, since the alg should rank only unrated items (unless in filter list)
@@ -47,7 +55,7 @@ class TestNXPageRank(TestCase):
 
         # rank with n_recs specified
         n_recs = 1
-        res_n_recs = alg.rank({'A000'}, self.graph, n_recs)
+        res_n_recs = alg.rank({'A000'}, self.graph, test_set, n_recs)
         self.assertEqual(len(res_n_recs), n_recs)
         item_rated_set = set([interaction.item_id for interaction in ratings.get_user_interactions("A000")])
         item_ranked_set = set([pred_interaction.item_id for pred_interaction in res_n_recs])
@@ -57,18 +65,18 @@ class TestNXPageRank(TestCase):
 
         # test personalized
         alg = NXPageRank(personalized=True)
-        result_personalized = alg.rank({'A000'}, self.graph)
+        result_personalized = alg.rank({'A000'}, self.graph, test_set)
 
         alg = NXPageRank()
-        result_not_personalized = alg.rank({'A000'}, self.graph)
+        result_not_personalized = alg.rank({'A000'}, self.graph, test_set)
 
         self.assertNotEqual(result_personalized, result_not_personalized)
 
         # test with custom parameters
         alg = NXPageRank(alpha=0.9, max_iter=4, tol=1e-3)
-        result_custom = alg.rank({'A000'}, self.graph)
+        result_custom = alg.rank({'A000'}, self.graph, test_set)
 
         alg = NXPageRank()
-        result_not_custom = alg.rank({'A000'}, self.graph)
+        result_not_custom = alg.rank({'A000'}, self.graph, test_set)
 
         self.assertNotEqual(result_custom, result_not_custom)
