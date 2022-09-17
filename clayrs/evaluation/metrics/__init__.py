fb0b0b310c7f807258b5d3d74990ea3b84d5131c
Silleellie <antonio.silletti@gmail.com>
2022-09-17 11:19:26 +0200

    implemented MAP and MAP@K

--- clayrs/evaluation/metrics/__init__.py
+++ clayrs/evaluation/metrics/__init__.py
@@ -3,4 +3,4 @@ from .classification_metrics import Precision, PrecisionAtK, RPrecision, Recall,
 from .error_metrics import MAE, MSE, RMSE
 from .fairness_metrics import GiniIndex, DeltaGap, PredictionCoverage, CatalogCoverage
 from .plot_metrics import PopRatioProfileVsRecs, PopRecsCorrelation, LongTailDistr
-from .ranking_metrics import NDCG, NDCGAtK, MRR, MRRAtK, Correlation
+from .ranking_metrics import NDCG, NDCGAtK, MRR, MRRAtK, Correlation, MAP
