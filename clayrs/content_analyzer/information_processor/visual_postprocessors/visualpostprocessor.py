1d12d89a42d3748628a8c47ed1c09a3ab7664fd7
Silleellie <antonio.silletti@gmail.com>
2022-11-21 20:52:43 +0100

    initial implementation KMeans

--- clayrs/content_analyzer/information_processor/visual_postprocessors/visualpostprocessor.py
+++ clayrs/content_analyzer/information_processor/visual_postprocessors/visualpostprocessor.py
@@ -1,7 +1,8 @@
 import inspect
 from abc import ABC, abstractmethod
 from sklearn.decomposition import PCA
-from typing import List
+from sklearn.cluster import KMeans
+from typing import List, Any
 import numpy as np
 
 from clayrs.utils.automatic_methods import autorepr
@@ -34,6 +35,24 @@ class DimensionalityReduction(VisualPostProcessor):
         raise NotImplementedError
 
 
+class SkLearnKMeans(VisualPostProcessor):
+
+    def __init__(self, n_clusters: Any = 8, init: Any = "k-means++", n_init: Any = 10, max_iter: Any = 300,
+                 tol: Any = 1e-4, random_state: Any = None, copy_x: Any = True, algorithm: Any = "auto"):
+        self.k_means = KMeans(n_clusters=n_clusters, init=init, n_init=n_init, max_iter=max_iter, tol=tol,
+                              random_state=random_state, copy_x=copy_x, algorithm=algorithm)
+        self._repr_string = autorepr(self, inspect.currentframe())
+
+    def process(self, field_repr_list: List[FieldRepresentation]) -> List[FieldRepresentation]:
+        return self.k_means.fit_transform(np.stack(field_repr_list, axis=0))
+
+    def __str__(self):
+        return "SkLearnKMeans"
+
+    def __repr__(self):
+        return self._repr_string
+
+
 class SkLearnPCA(DimensionalityReduction):
 
     def __init__(self, n_components=None, copy=True, whiten=False, svd_solver='auto', tol=0.0,
@@ -44,7 +63,7 @@ class SkLearnPCA(DimensionalityReduction):
         self._repr_string = autorepr(self, inspect.currentframe())
 
     def process(self, field_repr_list: List[EmbeddingField]) -> List[EmbeddingField]:
-        return self.pca.fit_transform(np.stack(field_repr_list, axis=0))
+        return self.pca.fit_transform(np.stack([e.value for e in field_repr_list], axis=0))
 
     def __str__(self):
         return 'SkLearnPca'
