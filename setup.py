299d58e5fdc5066edf826e88596a55f2316db9f6
Antonio Silletti <antonio.silletti@gmail.com>
2022-11-22 11:27:27 +0100

    Updated version to v0.3.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('requirements.txt') as f:
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-VERSION = "0.3.0"
+VERSION = "0.3.1"
 
 setup(name='clayrs',
       version=VERSION,
