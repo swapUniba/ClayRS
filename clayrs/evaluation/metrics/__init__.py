c91ce309f65dcf1bf98d5512622ae7fbce376ba8
m-elio <elio.musacchio28@gmail.com>
2023-01-11 15:34:03 +0100

    roc curve implemented as plot metric

--- clayrs/evaluation/metrics/__init__.py
+++ clayrs/evaluation/metrics/__init__.py
@@ -2,5 +2,5 @@ from .classification_metrics import Precision, PrecisionAtK, RPrecision, Recall,
     FMeasure, FMeasureAtK
 from .error_metrics import MAE, MSE, RMSE
 from .fairness_metrics import GiniIndex, DeltaGap, PredictionCoverage, CatalogCoverage
-from .plot_metrics import PopRatioProfileVsRecs, PopRecsCorrelation, LongTailDistr
+from .plot_metrics import PopRatioProfileVsRecs, PopRecsCorrelation, LongTailDistr, RocCurveAucScore
 from .ranking_metrics import NDCG, NDCGAtK, MRR, MRRAtK, Correlation, MAP
