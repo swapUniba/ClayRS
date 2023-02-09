df021606a140357b1a76057322229aaaed1441c7
Silleellie <antonio.silletti@gmail.com>
2023-02-09 12:29:34 +0100

    added test for different users appear in different split
    Co-authored-by: m-elio <m-elio@users.noreply.github.com>

--- test/evaluation/eval_pipeline_modules/test_metric_evaluator.py
+++ test/evaluation/eval_pipeline_modules/test_metric_evaluator.py
@@ -209,6 +209,89 @@ class TestMetricEvaluator(TestCase):
         # the sys result frame must contain results for the system of the Precision, Recall and MAP
         self.assertEqual(list(sys_result.columns), ['Precision - macro', 'Recall - macro', 'MAP'])
 
+    def test_eval_metrics_different_users_different_split(self):
+        rank1 = pd.DataFrame({
+            'user_id': ['u1', 'u1', 'u1', 'u1', 'u1', 'u1', 'u1',
+                        'u2', 'u2', 'u2', 'u2', 'u2', 'u2', 'u2'],
+            'item_id': ['i9', 'i6', 'inew1', 'inew2', 'i2', 'i1', 'i8',
+                        'i10', 'inew3', 'i2', 'i1', 'i8', 'i4', 'i9'],
+
+            'score': [500, 450, 400, 350, 300, 200, 150,
+                      400, 300, 200, 100, 50, 25, 10]
+        })
+        rank1 = Rank.from_dataframe(rank1)
+        rank1.item_map.append(['inew1', 'inew2', 'inew3'])
+
+        truth1 = pd.DataFrame({
+            'user_id': ['u1', 'u1', 'u1', 'u1', 'u1',
+                        'u2', 'u2', 'u2', 'u2', 'u2'],
+            'item_id': ['i1', 'i2', 'i6', 'i8', 'i9',
+                        'i1', 'i2', 'i4', 'i9', 'i10'],
+
+            'score': [3, 3, 4, 1, 1,
+                      5, 3, 3, 4, 4]
+        })
+        truth1 = Ratings.from_dataframe(truth1)
+
+        rank2 = pd.DataFrame({
+            'user_id': ['u2', 'u2', 'u2', 'u2', 'u2', 'u2', 'u2',
+                        'u3', 'u3', 'u3', 'u3', 'u3', 'u3', 'u3'],
+            'item_id': ['i9', 'i6', 'inew1', 'inew2', 'i2', 'i1', 'i8',
+                        'i10', 'inew3', 'i2', 'i1', 'i8', 'i4', 'i9'],
+
+            'score': [500, 450, 400, 350, 300, 200, 150,
+                      400, 300, 200, 100, 50, 25, 10]
+        })
+        rank2 = Rank.from_dataframe(rank2)
+        rank2.item_map.append(['inew1', 'inew2', 'inew3'])
+
+        truth2 = pd.DataFrame({
+            'user_id': ['u2', 'u2', 'u2', 'u2', 'u2',
+                        'u3', 'u3', 'u3', 'u3', 'u3'],
+            'item_id': ['i1', 'i2', 'i6', 'i8', 'i9',
+                        'i1', 'i2', 'i4', 'i9', 'i10'],
+
+            'score': [3, 3, 4, 1, 1,
+                      5, 3, 3, 4, 4]
+        })
+        truth2 = Ratings.from_dataframe(truth2)
+
+        rank_list = [rank1, rank2]
+        truth_list = [truth1, truth2]
+
+        sys_result, users_results = MetricEvaluator(rank_list, truth_list).eval_metrics([Precision(), Recall(), MAP()])
+        
+        # users in both frames are present
+        self.assertEqual({'u1', 'u2', 'u3'}, set(users_results.index))
+        self.assertEqual(list(users_results.columns), ['Precision - macro', 'Recall - macro', 'AP'])
+
+        # u2 is the only one that appears in both splits: the final result will contain the mean result for u2 across
+        # the two splits
+        u2_precision_split1 = float(Precision().perform(Split(rank1, truth1)).query("user_id=='u2'")["Precision - macro"])
+        u2_precision_split2 = float(Precision().perform(Split(rank2, truth2)).query("user_id=='u2'")["Precision - macro"])
+        u2_precision_expected = (u2_precision_split1 + u2_precision_split2) / 2
+
+        self.assertAlmostEqual(u2_precision_expected, float(users_results.query("user_id=='u2'")["Precision - macro"]))
+
+        u2_recall_split1 = float(Recall().perform(Split(rank1, truth1)).query("user_id=='u2'")["Recall - macro"].values[0])
+        u2_recall_split2 = float(Recall().perform(Split(rank2, truth2)).query("user_id=='u2'")["Recall - macro"].values[0])
+        u2_recall_expected = (u2_recall_split1 + u2_recall_split2) / 2
+
+        self.assertAlmostEqual(u2_recall_expected, float(users_results.query("user_id=='u2'")["Recall - macro"]))
+
+        u2_ap_split1 = float(MAP().perform(Split(rank1, truth1)).query("user_id=='u2'")["AP"].values[0])
+        u2_ap_split2 = float(MAP().perform(Split(rank2, truth2)).query("user_id=='u2'")["AP"].values[0])
+        u2_ap_expected = (u2_ap_split1 + u2_ap_split2) / 2
+
+        self.assertAlmostEqual(u2_ap_expected, float(users_results.query("user_id=='u2'")["AP"]))
+
+        # the sys_result frame must contain result of the system for each fold (2 in this case) + the mean result
+        self.assertTrue(len(sys_result) == 3)
+        self.assertEqual({'sys - fold1', 'sys - fold2', 'sys - mean'}, set(sys_result.index))
+
+        # the sys result frame must contain results for the system of the Precision, Recall and MAP
+        self.assertEqual(list(sys_result.columns), ['Precision - macro', 'Recall - macro', 'MAP'])
+
     @classmethod
     def tearDownClass(cls) -> None:
         os.remove('long_tail_distr_truth.png')
