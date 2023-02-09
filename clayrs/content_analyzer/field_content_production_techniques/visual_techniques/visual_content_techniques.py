7b44dfca5693aec435461a585279e70c72fda5e9
Silleellie <antonio.silletti@gmail.com>
2023-02-09 20:33:28 +0100

    implementation refinement
    Co-authored-by: m-elio <m-elio@users.noreply.github.com>

--- clayrs/content_analyzer/field_content_production_techniques/visual_techniques/visual_content_techniques.py
+++ clayrs/content_analyzer/field_content_production_techniques/visual_techniques/visual_content_techniques.py
@@ -40,8 +40,14 @@ class ClasslessImageFolder(Dataset):
     In this case, since labels are not of interest, only the image in the form of a Torch tensor will be returned.
     """
     def __init__(self, root, resize_size: Tuple[int, int], all_images_list: list = None):
-        self.image_paths = [os.path.join(root, file_name) for file_name in all_images_list]
+
+        if all_images_list is None:
+            self.image_paths = [os.path.join(root, file_name) for file_name in os.listdir(root)]
+        else:
+            self.image_paths = [os.path.join(root, file_name) for file_name in all_images_list]
+
         self.resize_size = list(resize_size)
+        self.count = 0
 
     def __getitem__(self, index):
         image_path = self.image_paths[index]
@@ -51,11 +57,10 @@ class ClasslessImageFolder(Dataset):
             x = TF.to_tensor(x)
             x = TF.resize(x, self.resize_size)
         except FileNotFoundError:
+            self.count += 1
             x = torch.zeros((3, self.resize_size[0], self.resize_size[1]))
 
-        y = 0
-
-        return x, y
+        return x
 
     def __len__(self):
         return len(self.image_paths)
@@ -99,12 +104,13 @@ class VisualContentTechnique(FieldContentProductionTechnique):
     """
 
     def __init__(self, imgs_dirs: str = "imgs_dirs", max_timeout: int = 2, max_retries: int = 5,
-                 max_workers: int = 0, resize_size: Tuple[int, int] = (100, 100)):
+                 max_workers: int = 0, batch_size: int = 64, resize_size: Tuple[int, int] = (227, 227)):
 
         self.imgs_dirs = imgs_dirs
         self.max_timeout = max_timeout
         self.max_retries = max_retries
         self.max_workers = max_workers
+        self.batch_size = batch_size
         self.resize_size = resize_size
 
     @staticmethod
@@ -195,17 +201,19 @@ class VisualContentTechnique(FieldContentProductionTechnique):
         ds = ClasslessImageFolder(root=field_images_dir,
                                   all_images_list=[content[field_name].split('/')[-1] for content in raw_source],
                                   resize_size=self.resize_size)
-        dl = DataLoader(ds, batch_size=32)
+        dl = DataLoader(ds, batch_size=self.batch_size, shuffle=False)
 
         return dl
 
+    @abstractmethod
     def produce_content(self, field_name: str, preprocessor_list: List[InformationProcessor],
                         postprocessor_list: List[EmbeddingInputPostProcessor],
                         source: RawInformationSource) -> List[FieldRepresentation]:
-        pass
+        raise NotImplementedError
 
+    @abstractmethod
     def __repr__(self):
-        pass
+        raise NotImplementedError
 
 
 class LowLevelVisual(VisualContentTechnique):
@@ -229,7 +237,7 @@ class LowLevelVisual(VisualContentTechnique):
 
         with get_progbar(dl) as pbar:
 
-            for (data_batch, _) in pbar:
+            for data_batch in pbar:
                 pbar.set_description(f"Processing and producing contents with {self}")
 
                 for content_data in data_batch:
@@ -264,11 +272,13 @@ class HighLevelVisual(VisualContentTechnique):
         representation_list: [FieldRepresentation] = []
 
         with get_progbar(dl) as pbar:
-            for (data_batch, _) in pbar:
+            for data_batch in pbar:
                 pbar.set_description(f"Processing and producing contents with {self}")
 
                 processed_data = self.process_data(data_batch, preprocessor_list)
                 representation_list.extend(self.produce_batch_repr(processed_data))
+
+        representation_list = self.postprocess_representations(representation_list, postprocessor_list)
 
         return representation_list
 
@@ -289,35 +299,48 @@ class PytorchImageModels(HighLevelVisual):
         flatten: whether the features obtained from the model should be flattened or not
     """
 
-    def __init__(self, model_name: str, feature_layer: int = -1):
-        super().__init__()
-        self.model = timm.create_model(model_name, pretrained=True, features_only=True)
+    def __init__(self, model_name: str, feature_layer: int = -1, flatten=True,
+                 imgs_dirs: str = "imgs_dirs", max_timeout: int = 2, max_retries: int = 5,
+                 max_workers: int = 0, batch_size: int = 64, resize_size: Tuple[int, int] = (227, 227)):
+
+        super().__init__(imgs_dirs, max_timeout, max_retries, max_workers, batch_size, resize_size)
+        self.model = timm.create_model(model_name, pretrained=True, features_only=True).eval()
         self.feature_layer = feature_layer
+        self.flatten = flatten
 
     def produce_batch_repr(self, field_data: torch.Tensor) -> List[FieldRepresentation]:
 
         if len(field_data.shape) == 3:
             field_data = field_data.unsqueeze(dim=1)
 
-        return list(map(lambda x: EmbeddingField(x.detach().numpy()),
-                        self.model(field_data.squeeze())[self.feature_layer]))
+        if self.flatten:
+            return list(map(lambda x: EmbeddingField(x.detach().numpy().flatten()),
+                            self.model(field_data.squeeze())[self.feature_layer]))
+        else:
+            return list(map(lambda x: EmbeddingField(x.detach().numpy()),
+                            self.model(field_data.squeeze())[self.feature_layer]))
 
     def __str__(self):
-        return "PytorchImageModels"
+        return "Pytorch Image Models"
+
+    def __repr__(self):
+        return "Pytorch Image Models"
 
 
 class SkImageHogDescriptor(LowLevelVisual):
 
-    def __init__(self, orientations=9, pixels_per_cell=(8, 8), cells_per_block=(3, 3), block_norm='L2-Hys',
-                 transform_sqrt=False, flatten=False):
+    def __init__(self, orientations=9, pixels_per_cell=(8, 8), cells_per_block=(3, 3),
+                 block_norm='L2-Hys', transform_sqrt=False, flatten=False,
+                 imgs_dirs: str = "imgs_dirs", max_timeout: int = 2, max_retries: int = 5,
+                 max_workers: int = 0, batch_size: int = 64, resize_size: Tuple[int, int] = (227, 227)):
+
+        super().__init__(imgs_dirs, max_timeout, max_retries, max_workers, batch_size, resize_size)
         self.hog = lambda x, channel_axis: hog(x, orientations=orientations, pixels_per_cell=pixels_per_cell,
                                                cells_per_block=cells_per_block, block_norm=block_norm,
-                                               transform_sqrt=transform_sqrt, feature_vector=True,
+                                               transform_sqrt=transform_sqrt, feature_vector=flatten,
                                                channel_axis=channel_axis)
-        self.flatten = flatten
 
         self._repr_string = autorepr(self, inspect.currentframe())
-        super().__init__()
 
     def produce_single_repr(self, field_data: torch.Tensor) -> FieldRepresentation:
 
@@ -328,7 +351,6 @@ class SkImageHogDescriptor(LowLevelVisual):
             channel_axis = 0
 
         hog_features = self.hog(field_data.numpy(), channel_axis=channel_axis)
-        hog_features = hog_features.flatten() if self.flatten else hog_features
         return EmbeddingField(hog_features)
 
     def __str__(self):
@@ -405,7 +427,7 @@ class SkImageMainColors(LowLevelVisual):
             field_data = field_data.squeeze()
 
         if field_data.shape[0] != 3:
-            raise Exception('Image not in RGB')
+            field_data = field_data.repeat(3, 1, 1)
 
         numpy_field_data = field_data.numpy() * 255
         colors = np.array([numpy_field_data[0, :, :].flatten(),
@@ -437,7 +459,7 @@ class SkImageColorQuantization(LowLevelVisual):
             field_data = field_data.squeeze()
 
         if field_data.shape[0] != 3:
-            raise Exception('Image in grey scale')
+            raise ValueError('Image in grey scale')
 
         numpy_field_data = field_data.numpy() * 255
         numpy_field_data = numpy_field_data.reshape((numpy_field_data.shape[1] * numpy_field_data.shape[2], 3))
