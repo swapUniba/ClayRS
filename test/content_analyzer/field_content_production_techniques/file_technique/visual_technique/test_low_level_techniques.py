04e5bd847b7a686fbe59c7b03a923b2b6c6d3512
m-elio <elio.musacchio28@gmail.com>
2023-11-28 22:37:39 +0100

    updated tests for low level techniques on images

--- test/content_analyzer/field_content_production_techniques/file_technique/visual_technique/test_low_level_techniques.py
+++ test/content_analyzer/field_content_production_techniques/file_technique/visual_technique/test_low_level_techniques.py
@@ -4,23 +4,23 @@ import unittest
 
 import PIL.Image
 import numpy as np
-import torchvision.transforms.functional as TF
 import torch
+import torchvision.transforms.functional as TF
 from scipy.ndimage import convolve
 from skimage.feature import hog, canny, SIFT, local_binary_pattern
 from sklearn.cluster import KMeans
 
 from clayrs.content_analyzer import JSONFile, SkImageHogDescriptor, EmbeddingField, SkImageCannyEdgeDetector, \
     TorchGrayscale, ColorsHist, ColorQuantization, SkImageSIFT, SkImageLBP, CustomFilterConvolution
-from test.content_analyzer.field_content_production_techniques.visual_technique.test_visual_content_technique import \
-    create_full_path_source, ds_path_without_field_name
+from test.content_analyzer.field_content_production_techniques.file_technique.visual_technique.test_visual_content_technique import \
+    create_full_path_source, ds_path_without_field_name, raw_source_path_local_rel, images_path
 
 
 class TestLowLevelTechnique(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls) -> None:
-        full_path_source = create_full_path_source()
+        full_path_source = create_full_path_source(raw_source_path_local_rel, 'imagePath', images_path)
 
         # pre-build the list of images from the source, this is done for
         # comparing outputs obtained from the techniques
@@ -29,12 +29,10 @@ class TestLowLevelTechnique(unittest.TestCase):
         images_list = []
         for data in new_source:
             image_path = data['imagePath']
-            image = PIL.Image.open(image_path).convert('RGB')
-            image = TF.resize(TF.to_tensor(image), [227, 227])
-            image = image.unsqueeze(0)
+            image = torch.from_numpy(np.array(PIL.Image.open(image_path).convert('RGB'))).moveaxis(-1, 0)
             images_list.append(image)
 
-        cls.images_list = torch.vstack(images_list)
+        cls.images_list = images_list
         cls.full_path_source = full_path_source
 
     @classmethod
@@ -54,11 +52,11 @@ class TestHogDescriptor(TestLowLevelTechnique):
         # hog descriptor from skimage provides the argument "feature_vector" to output a 1 dimensional
         # representation of the descriptors, since the parameter is true by default, we first test
         # for the "flatten" set to true case
-        technique = SkImageHogDescriptor(flatten=True, imgs_dirs=ds_path_without_field_name)
-        framework_output = technique.produce_content('imagePath', [], [], source)
+        technique = SkImageHogDescriptor(flatten=True, contents_dirs=ds_path_without_field_name)
+        framework_output = technique.produce_content('imagePath', [], source)
 
-        # check that there are 5 outputs and all of them match the expected number of dimensions
-        self.assertEqual(5, len(framework_output))
+        # check that there are 3 outputs and all of them match the expected number of dimensions
+        self.assertEqual(3, len(framework_output))
         self.assertTrue(all(isinstance(out, EmbeddingField) for out in framework_output))
         self.assertTrue(all(len(out.value.shape) == 1 for out in framework_output))
 
@@ -76,8 +74,8 @@ class TestHogDescriptor(TestLowLevelTechnique):
         self.assertEqual(1, len(single_out.value.shape))
 
         # check that output is NOT flattened when "flatten" parameter is set to false
-        technique = SkImageHogDescriptor(flatten=False, imgs_dirs=ds_path_without_field_name)
-        framework_output = technique.produce_content('imagePath', [], [], source)
+        technique = SkImageHogDescriptor(flatten=False, contents_dirs=ds_path_without_field_name)
+        framework_output = technique.produce_content('imagePath', [], source)
 
         self.assertTrue(all(len(out.value.shape) == 5 for out in framework_output))
 
@@ -91,11 +89,11 @@ class TestCannyEdgeDetector(TestLowLevelTechnique):
         source = JSONFile(self.full_path_source)
 
         # NOTE: canny edge detector works on grayscale images
-        technique = SkImageCannyEdgeDetector(flatten=False, imgs_dirs=ds_path_without_field_name)
-        framework_output = technique.produce_content('imagePath', [TorchGrayscale()], [], source)
+        technique = SkImageCannyEdgeDetector(flatten=False, contents_dirs=ds_path_without_field_name)
+        framework_output = technique.produce_content('imagePath', [TorchGrayscale()], source)
 
-        # check that there are 5 outputs and all of them match the expected number of dimensions
-        self.assertEqual(5, len(framework_output))
+        # check that there are 3 outputs and all of them match the expected number of dimensions
+        self.assertEqual(3, len(framework_output))
         self.assertTrue(all(isinstance(out, EmbeddingField) for out in framework_output))
         self.assertTrue(all(len(out.value.shape) == 2 for out in framework_output))
 
@@ -114,8 +112,8 @@ class TestCannyEdgeDetector(TestLowLevelTechnique):
         self.assertEqual(2, len(single_out.value.shape))
 
         # check that output is flattened when "flatten" parameter is set to true
-        technique = SkImageCannyEdgeDetector(flatten=True, imgs_dirs=ds_path_without_field_name)
-        framework_output = technique.produce_content('imagePath', [TorchGrayscale()], [], source)
+        technique = SkImageCannyEdgeDetector(flatten=True, contents_dirs=ds_path_without_field_name)
+        framework_output = technique.produce_content('imagePath', [TorchGrayscale()], source)
 
         self.assertTrue(all(len(out.value.shape) == 1 for out in framework_output))
 
@@ -132,18 +130,18 @@ class TestColorsHist(TestLowLevelTechnique):
 
         source = JSONFile(self.full_path_source)
 
-        technique = ColorsHist(imgs_dirs=ds_path_without_field_name)
-        framework_output = technique.produce_content('imagePath', [], [], source)
+        technique = ColorsHist(contents_dirs=ds_path_without_field_name)
+        framework_output = technique.produce_content('imagePath', [], source)
 
-        # check that there are 5 outputs and all of them match the expected number of dimensions
-        self.assertEqual(5, len(framework_output))
+        # check that there are 3 outputs and all of them match the expected number of dimensions
+        self.assertEqual(3, len(framework_output))
         self.assertTrue(all(isinstance(out, EmbeddingField) for out in framework_output))
         self.assertTrue(all(len(out.value.shape) == 2 for out in framework_output))
 
         # instantiate the original technique and compare its outputs with the ones obtained by the framework
         color_output = []
         for image in self.images_list:
-            image = image.numpy() * 255
+            image = image.numpy()
             colors = np.array([image[0, :, :].flatten(),
                                image[1, :, :].flatten(),
                                image[2, :, :].flatten()])
@@ -171,17 +169,17 @@ class TestColorQuantization(TestLowLevelTechnique):
 
         source = JSONFile(self.full_path_source)
 
-        technique = ColorQuantization(n_colors=3, flatten=False, random_state=42, imgs_dirs=ds_path_without_field_name)
-        framework_output = technique.produce_content('imagePath', [], [], source)
+        technique = ColorQuantization(n_colors=3, flatten=False, random_state=42, contents_dirs=ds_path_without_field_name)
+        framework_output = technique.produce_content('imagePath', [], source)
 
-        # check that there are 5 outputs and all of them match the expected number of dimensions
+        # check that there are 3 outputs and all of them match the expected number of dimensions
         # in this case the shape is also explicitly checked (since 3 colors are considered it is expected that each
-        # output will be similar to [20, 134, 25] where each value represents one of the image channels;
+        # output will be similar to [20, 134, 23] where each value represents one of the image channels;
         # so the full output should be similar to
-        # [[20, 134, 25],
-        # [56, 72, 3],
-        # [9, 25, 39]])
-        self.assertEqual(5, len(framework_output))
+        # [[20, 134, 23],
+        # [36, 72, 3],
+        # [9, 23, 39]])
+        self.assertEqual(3, len(framework_output))
         self.assertTrue(all(isinstance(out, EmbeddingField) for out in framework_output))
         self.assertTrue(all(len(out.value.shape) == 2 for out in framework_output))
         self.assertTrue(all(out.value.shape == (3, 3) for out in framework_output))
@@ -191,7 +189,7 @@ class TestColorQuantization(TestLowLevelTechnique):
         k_means = KMeans(3, random_state=42)
 
         for image in self.images_list:
-            reshaped_image = (image.numpy() * 255).reshape(image.shape[1] * image.shape[2], 3)
+            reshaped_image = (image.numpy()).reshape(image.shape[1] * image.shape[2], 3)
             k_means.fit(reshaped_image)
             quantization_output.append(k_means.cluster_centers_.astype(int))
 
@@ -204,8 +202,8 @@ class TestColorQuantization(TestLowLevelTechnique):
         self.assertEqual(2, len(single_out.value.shape))
 
         # check that output is flattened when "flatten" parameter is set to true
-        technique = ColorQuantization(flatten=True, imgs_dirs=ds_path_without_field_name)
-        framework_output = technique.produce_content('imagePath', [], [], source)
+        technique = ColorQuantization(flatten=True, contents_dirs=ds_path_without_field_name)
+        framework_output = technique.produce_content('imagePath', [], source)
 
         self.assertTrue(all(len(out.value.shape) == 1 for out in framework_output))
 
@@ -223,11 +221,11 @@ class TestSkImageSIFT(TestLowLevelTechnique):
         source = JSONFile(self.full_path_source)
 
         # NOTE: sift works on grayscale images
-        technique = SkImageSIFT(flatten=False, imgs_dirs=ds_path_without_field_name)
-        framework_output = technique.produce_content('imagePath', [TorchGrayscale()], [], source)
+        technique = SkImageSIFT(flatten=False, contents_dirs=ds_path_without_field_name)
+        framework_output = technique.produce_content('imagePath', [TorchGrayscale()], source)
 
-        # check that there are 5 outputs and all of them match the expected number of dimensions
-        self.assertEqual(5, len(framework_output))
+        # check that there are 3 outputs and all of them match the expected number of dimensions
+        self.assertEqual(3, len(framework_output))
         self.assertTrue(all(isinstance(out, EmbeddingField) for out in framework_output))
         self.assertTrue(all(len(out.value.shape) == 2 for out in framework_output))
 
@@ -249,8 +247,8 @@ class TestSkImageSIFT(TestLowLevelTechnique):
         self.assertEqual(2, len(single_out.value.shape))
 
         # check that output is flattened when "flatten" parameter is set to true
-        technique = SkImageSIFT(flatten=True, imgs_dirs=ds_path_without_field_name)
-        framework_output = technique.produce_content('imagePath', [TorchGrayscale()], [], source)
+        technique = SkImageSIFT(flatten=True, contents_dirs=ds_path_without_field_name)
+        framework_output = technique.produce_content('imagePath', [TorchGrayscale()], source)
 
         self.assertTrue(all(len(out.value.shape) == 1 for out in framework_output))
 
@@ -274,11 +272,11 @@ class TestSkImageLBP(TestLowLevelTechnique):
 
         # as_image set to True
         # NOTE: lbp technique works on grayscale images
-        technique = SkImageLBP(p=8, r=2, as_image=True, imgs_dirs=ds_path_without_field_name)
-        framework_output = technique.produce_content('imagePath', [TorchGrayscale()], [], source)
+        technique = SkImageLBP(p=8, r=2, as_image=True, contents_dirs=ds_path_without_field_name)
+        framework_output = technique.produce_content('imagePath', [TorchGrayscale()], source)
 
-        # check that there are 5 outputs and all of them match the expected number of dimensions
-        self.assertEqual(5, len(framework_output))
+        # check that there are 3 outputs and all of them match the expected number of dimensions
+        self.assertEqual(3, len(framework_output))
         self.assertTrue(all(isinstance(out, EmbeddingField) for out in framework_output))
         self.assertTrue(all(len(out.value.shape) == 2 for out in framework_output))
 
@@ -293,11 +291,11 @@ class TestSkImageLBP(TestLowLevelTechnique):
             np.testing.assert_array_equal(single_f_out.value, single_l_out)
 
         # as_image set to False
-        technique = SkImageLBP(p=8, r=2, as_image=False, imgs_dirs=ds_path_without_field_name)
-        framework_output = technique.produce_content('imagePath', [TorchGrayscale()], [], source)
+        technique = SkImageLBP(p=8, r=2, as_image=False, contents_dirs=ds_path_without_field_name)
+        framework_output = technique.produce_content('imagePath', [TorchGrayscale()], source)
 
-        # check that there are 5 outputs and all of them match the expected number of dimensions
-        self.assertEqual(5, len(framework_output))
+        # check that there are 3 outputs and all of them match the expected number of dimensions
+        self.assertEqual(3, len(framework_output))
         self.assertTrue(all(isinstance(out, EmbeddingField) for out in framework_output))
         self.assertTrue(all(len(out.value.shape) == 1 for out in framework_output))
 
@@ -319,8 +317,8 @@ class TestSkImageLBP(TestLowLevelTechnique):
         self.assertEqual(1, len(single_out.value.shape))
 
         # check that output is flattened when "flatten" parameter is set to true and "as_image" is set to True
-        technique = SkImageLBP(imgs_dirs=ds_path_without_field_name, as_image=True, flatten=True, p=8, r=2)
-        framework_output = technique.produce_content('imagePath', [TorchGrayscale()], [], source)
+        technique = SkImageLBP(contents_dirs=ds_path_without_field_name, as_image=True, flatten=True, p=8, r=2)
+        framework_output = technique.produce_content('imagePath', [TorchGrayscale()], source)
 
         self.assertTrue(all(len(out.value.shape) == 1 for out in framework_output))
 
@@ -329,8 +327,8 @@ class TestSkImageLBP(TestLowLevelTechnique):
         self.assertEqual(1, len(single_out.value.shape))
 
         # check that output is flattened when "flatten" parameter is set to true and "as_image" is set to False
-        technique = SkImageLBP(imgs_dirs=ds_path_without_field_name, as_image=False, flatten=True, p=8, r=2)
-        framework_output = technique.produce_content('imagePath', [TorchGrayscale()], [], source)
+        technique = SkImageLBP(contents_dirs=ds_path_without_field_name, as_image=False, flatten=True, p=8, r=2)
+        framework_output = technique.produce_content('imagePath', [TorchGrayscale()], source)
 
         self.assertTrue(all(len(out.value.shape) == 1 for out in framework_output))
 
@@ -353,11 +351,11 @@ class TestCustomFilterConvolution(TestLowLevelTechnique):
         ])
 
         # NOTE: custom filter convolution technique works on grayscale images
-        technique = CustomFilterConvolution(weights, flatten=False, imgs_dirs=ds_path_without_field_name)
-        framework_output = technique.produce_content('imagePath', [TorchGrayscale()], [], source)
+        technique = CustomFilterConvolution(weights, flatten=False, contents_dirs=ds_path_without_field_name)
+        framework_output = technique.produce_content('imagePath', [TorchGrayscale()], source)
 
-        # check that there are 5 outputs and all of them match the expected number of dimensions
-        self.assertEqual(5, len(framework_output))
+        # check that there are 3 outputs and all of them match the expected number of dimensions
+        self.assertEqual(3, len(framework_output))
         self.assertTrue(all(isinstance(out, EmbeddingField) for out in framework_output))
         self.assertTrue(all(len(out.value.shape) == 2 for out in framework_output))
 
@@ -378,8 +376,8 @@ class TestCustomFilterConvolution(TestLowLevelTechnique):
         self.assertEqual(2, len(single_out.value.shape))
 
         # check that output is flattened when "flatten" parameter is set to true
-        technique = CustomFilterConvolution(weights, flatten=True, imgs_dirs=ds_path_without_field_name)
-        framework_output = technique.produce_content('imagePath', [TorchGrayscale()], [], source)
+        technique = CustomFilterConvolution(weights, flatten=True, contents_dirs=ds_path_without_field_name)
+        framework_output = technique.produce_content('imagePath', [TorchGrayscale()], source)
 
         self.assertTrue(all(len(out.value.shape) == 1 for out in framework_output))
 
