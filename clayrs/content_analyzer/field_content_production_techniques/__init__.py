ca77d0f74295048dc15b3221023504702d9a2554
m-elio <elio.musacchio28@gmail.com>
2023-11-28 22:38:12 +0100

    updated visual_techniques -> file_techniques

--- clayrs/content_analyzer/field_content_production_techniques/__init__.py
+++ clayrs/content_analyzer/field_content_production_techniques/__init__.py
@@ -1,7 +1,7 @@
 from . import embedding_technique
 
 from .embedding_technique import *
-from .visual_techniques import *
+from .file_techniques import *
 from .tf_idf import WhooshTfIdf, SkLearnTfIdf
 from .field_content_production_technique import OriginalData, FromNPY
 from .synset_document_frequency import PyWSDSynsetDocumentFrequency
---
+++ clayrs/content_analyzer/field_content_production_techniques/file_techniques/audio_techniques/__init__.py
@@ -0,0 +1,2 @@
+from .low_level_techniques import MFCC
+from .high_level_techniques import TorchAudioPretrained, VGGISH
