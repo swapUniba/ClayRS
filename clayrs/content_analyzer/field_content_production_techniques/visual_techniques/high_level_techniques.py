2acb02b198184fe150a48ae7e3aae805d3a02eba
m-elio <elio.musacchio28@gmail.com>
2023-11-28 16:53:07 +0100

    restructured HighLevelVisual techniques and added FrameTechnique

--- clayrs/content_analyzer/field_content_production_techniques/visual_techniques/high_level_techniques.py
+++ clayrs/content_analyzer/field_content_production_techniques/visual_techniques/high_level_techniques.py
@@ -1,17 +1,23 @@
 from __future__ import annotations
 import inspect
+import os
 from abc import abstractmethod
-from collections import OrderedDict
 from pathlib import Path
-from typing import Tuple, List, TYPE_CHECKING, Callable
+from typing import List, TYPE_CHECKING, Callable, Tuple, Optional
 
+import cv2
 import timm
 import torch
-import cv2
 import numpy as np
+import torchvision.models.video as v_models
+import torchvision.models.optical_flow as opt_f_models
+from torchvision.models import get_model_weights
+from timm.models._helpers import load_checkpoint
+from torchvision.models.feature_extraction import create_feature_extractor
+from torchvision.transforms import Lambda, Compose, ToTensor
 
 from clayrs.content_analyzer.content_representation.content import EmbeddingField
-from clayrs.content_analyzer.field_content_production_techniques.visual_techniques.visual_content_techniques import \
+from clayrs.content_analyzer.field_content_production_techniques.file_techniques.visual_techniques.visual_content_techniques import \
     VisualContentTechnique
 from clayrs.utils.context_managers import get_progbar
 from clayrs.utils.automatic_methods import autorepr
@@ -19,41 +25,108 @@ from clayrs.utils.automatic_methods import autorepr
 if TYPE_CHECKING:
     from clayrs.content_analyzer.content_representation.content import FieldRepresentation
     from clayrs.content_analyzer.raw_information_source import RawInformationSource
-    from clayrs.content_analyzer.information_processor.information_processor_abstract import ImageProcessor
-    from clayrs.content_analyzer.information_processor.postprocessors.postprocessor import \
-        EmbeddingInputPostProcessor
+    from clayrs.content_analyzer.information_processor.preprocessors.information_processor_abstract import \
+        ImageProcessor
 
 
 class HighLevelVisual(VisualContentTechnique):
     """
     Technique which encapsulates the logic for all visual techniques that work at a high level, that is they work on
-    batches of images in an efficient way
+    batches of images in an efficient way by leveraging pre-trained models
     """
 
-    def __init__(self, imgs_dirs: str = "imgs_dirs", max_timeout: int = 2, max_retries: int = 5,
-                 max_workers: int = 0, batch_size: int = 64, resize_size: Tuple[int, int] = (227, 227)):
-        super().__init__(imgs_dirs, max_timeout, max_retries, max_workers, batch_size, resize_size)
+    def __init__(self, model, feature_layer: int = -1, flatten: bool = False, device: str = 'cpu',
+                 apply_on_output: Callable[[torch.Tensor], torch.Tensor] = None,
+                 contents_dirs: str = "contents_dirs", time_tuple: Tuple[Optional[int], Optional[int]] = (0, None),
+                 max_timeout: int = 2, max_retries: int = 5,
+                 max_workers: int = 0, batch_size: int = 64):
+        super().__init__(contents_dirs, time_tuple, max_timeout, max_retries, max_workers, batch_size)
 
-    def produce_content(self, field_name: str, preprocessor_list: List[ImageProcessor],
-                        postprocessor_list: List[EmbeddingInputPostProcessor],
-                        source: RawInformationSource) -> List[FieldRepresentation]:
-        dl = self.get_data_loader(field_name, source)
+        def return_self(x: torch.Tensor) -> torch.Tensor:
+            return x
+
+        self.apply_on_output = return_self if apply_on_output is None else apply_on_output
+
+        self.model = model
+        self.feature_layer = feature_layer
+        self.device = device
+        self.flatten = flatten
+
+    def apply_and_flatten(self, single_repr):
+
+        single_repr = self.apply_on_output(single_repr)
+        single_repr = single_repr.numpy()
+
+        if self.flatten:
+
+            # do not flatten if already flattened
+            if len(single_repr.shape) != 1:
+                # single global feature vector
+                if single_repr.shape[0] == 1:
+                    single_repr = single_repr.flatten()
+                # multiple local descriptors
+                else:
+                    single_repr = single_repr.reshape(single_repr.shape[0], -1)
+
+        return single_repr
+
+    @abstractmethod
+    def produce_batch_repr(self, field_data: torch.Tensor) -> torch.Tensor:
+        """
+        Method that will produce the corresponding complex representations from all field data for the batch
+        """
+        raise NotImplementedError
+
+
+class HighLevelVisualFrame(HighLevelVisual):
+    """
+    High level visual techniques are categorized into three subclasses:
+
+        - Frame level techniques
+        - Clip level techniques
+        - Flow level techniques
+
+    Frame level techniques return an output for each single image or single video frame,
+    so, for example, if resnet is used as technique, for each input frame the corresponding output
+    features will be extracted
+    """
+
+    def produce_content(self, field_name: str, preprocessor_list: List[ImageProcessor], source: RawInformationSource) -> List[FieldRepresentation]:
+
+        dl, video_mode = self.get_data_loader(field_name, source, preprocessor_list)
 
         representation_list: [FieldRepresentation] = []
 
         with get_progbar(dl) as pbar:
-            for data_batch in pbar:
+            for processed_data_batch in pbar:
                 pbar.set_description(f"Processing and producing contents with {self}")
 
-                processed_data = self.process_data(data_batch, preprocessor_list)
-                representation_list.extend(self.produce_batch_repr(processed_data))
+                if not video_mode:
+                    # preprocessing is done by the dataset directly in the dataloader
+                    reprs = self.produce_batch_repr(processed_data_batch).unsqueeze(1)
+                else:
 
-        representation_list = self.postprocess_representations(representation_list, postprocessor_list)
+                    processed_data_batch, idxs = processed_data_batch
+
+                    # clips generated from video
+                    if len(processed_data_batch.shape) == 5:
+                        frames_size = processed_data_batch.size()[1]
+                        processed_data_batch = torch.flatten(processed_data_batch, start_dim=0, end_dim=1)
+                        reprs = self.produce_batch_repr(processed_data_batch)
+                        reprs = torch.split(reprs, list(np.array(idxs) * frames_size))
+                    # entire video
+                    else:
+                        reprs = self.produce_batch_repr(processed_data_batch)
+                        reprs = torch.split(reprs, idxs)
+
+                for single_repr in reprs:
+                    out_f_applied_single_repr = self.apply_and_flatten(single_repr)
+                    representation_list.append(EmbeddingField(out_f_applied_single_repr))
 
         return representation_list
 
     @abstractmethod
-    def produce_batch_repr(self, field_data: torch.Tensor) -> List[FieldRepresentation]:
+    def produce_batch_repr(self, field_data: torch.Tensor) -> torch.Tensor:
         """
         Method that will produce the corresponding complex representations from all field data for the batch
         """
