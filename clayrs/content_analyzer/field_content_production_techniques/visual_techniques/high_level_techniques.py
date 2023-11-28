e624644a2824d2af57294b57aca05e7ec5da1ef9
m-elio <elio.musacchio28@gmail.com>
2023-11-28 16:53:52 +0100

    restructured PytorchImageModels class

--- clayrs/content_analyzer/field_content_production_techniques/visual_techniques/high_level_techniques.py
+++ clayrs/content_analyzer/field_content_production_techniques/visual_techniques/high_level_techniques.py
@@ -133,67 +133,78 @@ class HighLevelVisualFrame(HighLevelVisual):
         raise NotImplementedError
 
 
-class PytorchImageModels(HighLevelVisual):
+class PytorchImageModels(HighLevelVisualFrame):
     """
-    High level technique which uses the [timm library] (https://timm.fast.ai/) for feature extraction from images using
-    pre-trained models
+    High level technique which uses the [timm library] (https://timm.fast.ai/) for feature extraction from images or
+    video frames using pre-trained models
 
     Args:
         model_name: a model name supported by the timm library
         feature_layer: the layer index from which the features will be retrieved
             NOTE: the model is loaded from the timm library with the parameter "features_only" set at True, meaning
             that only feature layers of the model will be available and accessible through the index
+        custom_weights_path: path where the weights are stored locally, if not specified the default weights for the
+            model on the timm library will be used
+        use_default_transforms: if True, the default preprocessing operations for the model will be applied, otherwise
+            nothing will be done on the inputs. Defaults to False.
         flatten: whether the features obtained from the model should be flattened or not
-        imgs_dirs: directory where the images are stored (or will be stored in the case of fields containing links)
-        max_timeout: maximum time to wait before considering a request failed (image from link)
-        max_retries: maximum number of retries to retrieve an image from a link
+        device: device where the model will be stored ('cpu', 'cuda:0', ...). If not specified, 'cpu' will be set
+            by default
+        apply_on_output: custom lambda function to be applied to the features output
+        contents_dirs: directory where the files are stored (or will be stored in the case of fields containing links)
+        max_timeout: maximum time to wait before considering a request failed (file from link)
+        max_retries: maximum number of retries to retrieve a file from a link
         max_workers: maximum number of workers for parallelism
-        batch_size: batch size for the images dataloader
-        resize_size: since the Tensorflow dataset requires all images to be of the same size, they will all be resized
-            to the specified size. Note that if you were to specify a resize transformer in the preprocessing pipeline,
-            the size specified in the latter will be the final resize size
+        batch_size: batch size for the dataloader
     """
 
-    def __init__(self, model_name: str, feature_layer: int = -1, flatten: bool = True, device: str = 'cpu',
+    def __init__(self, model_name, feature_layer: int = -1, flatten: bool = False, device: str = 'cpu',
                  apply_on_output: Callable[[torch.Tensor], torch.Tensor] = None,
-                 imgs_dirs: str = "imgs_dirs", max_timeout: int = 2, max_retries: int = 5,
-                 max_workers: int = 0, batch_size: int = 64, resize_size: Tuple[int, int] = (227, 227)):
+                 contents_dirs: str = "contents_dirs", time_tuple: Tuple[Optional[int], Optional[int]] = (0, None),
+                 custom_weights_path: str = None,
+                 use_default_transforms: bool = False,
+                 num_classes: int = None,
+                 max_timeout: int = 2, max_retries: int = 5,
+                 max_workers: int = 0, batch_size: int = 64):
 
-        super().__init__(imgs_dirs, max_timeout, max_retries, max_workers, batch_size, resize_size)
-        original_model = timm.create_model(model_name, pretrained=True)
+        if custom_weights_path is None:
+            original_model = timm.create_model(model_name, pretrained=True)
+        else:
+            original_model = timm.create_model(model_name,
+                                               checkpoint_path=custom_weights_path,
+                                               num_classes=num_classes)
 
-        feature_layer = list(original_model._modules.keys())[feature_layer]
+        self.default_transform = None
 
-        layers = {}
-        for layer_name, layer in original_model._modules.items():
-            layers[layer_name] = layer
-            if layer_name == feature_layer:
-                break
+        if use_default_transforms:
+            default_transform = timm.data.create_transform(
+                **timm.data.resolve_data_config(original_model.pretrained_cfg))
+            default_transform = [x if type(x) != ToTensor else Lambda(lambda x: x.div(255.0)) for x in
+                                 default_transform.transforms]
+            self.default_transform = Compose(default_transform)
 
-        self.model = torch.nn.Sequential(OrderedDict(layers)).eval()
+        self.original_model = original_model
 
-        def return_self(x: torch.Tensor) -> torch.Tensor:
-            return x
+        if isinstance(feature_layer, int):
+            feature_layer = list(dict(original_model.named_modules()).keys())[feature_layer]
 
-        self.apply_on_output = return_self if apply_on_output is None else apply_on_output
+        model = create_feature_extractor(original_model, {feature_layer: "feature_layer"}).to(device).eval()
 
-        self.model.to(device)
-        self.device = device
-        self.flatten = flatten
+        super().__init__(model, feature_layer, flatten, device, apply_on_output, contents_dirs, time_tuple,
+                         max_timeout, max_retries, max_workers, batch_size)
+
         self.model_name = model_name
-
         self._repr_string = autorepr(self, inspect.currentframe())
 
-    def produce_batch_repr(self, field_data: torch.Tensor) -> List[EmbeddingField]:
+    @torch.no_grad()
+    def produce_batch_repr(self, field_data: torch.Tensor) -> torch.Tensor:
 
-        if self.flatten:
-            with torch.no_grad():
-                return list(map(lambda x: EmbeddingField(x.cpu().detach().numpy().flatten()),
-                                self.apply_on_output(self.model(field_data.to(self.device)))))
-        else:
-            with torch.no_grad():
-                return list(map(lambda x: EmbeddingField(x.cpu().detach().numpy()),
-                                self.apply_on_output(self.model(field_data.to(self.device)))))
+        if self.default_transform is not None:
+            field_data = self.default_transform(field_data)
+
+        out = self.model(field_data.to(self.device))['feature_layer']
+
+        return out.cpu()
 
     def __str__(self):
         return f"Pytorch Image Models ({self.model_name})"
