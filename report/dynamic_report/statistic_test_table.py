09e3b31f65262de6337b51d9c42ac0912bf3a550
kozen88 <93886188+kozen88@users.noreply.github.com>
2024-02-22 12:01:03 +0100

    documentation to statistic_test_table.py added

--- report/dynamic_report/statistic_test_table.py
+++ report/dynamic_report/statistic_test_table.py
@@ -3,14 +3,13 @@
 
 import pandas as pd
 import openpyxl
-
 from report.dynamic_report.support_reporting_lib import add_to_latex_file, replace_in_latex_file
 
 
 # funzione di supporto per la formattazione della tabella che mostra la rilevanza statistica
 # tra due sistemi messi a confronto
 def format_scientific_notation(value):
-    # Formatta l'esponente scientifico in modo più leggibile
+    # Format the value in scientific notation
     formatted_value = "{:.2e}".format(value)
     parts = formatted_value.split("e")
     return f"{parts[0]} \\times 10^{{{int(parts[1])}}}"
@@ -19,6 +18,24 @@ def format_scientific_notation(value):
 # Questa funzione preleva le informazioni inerenti ai sistemi messi a confronto e le stampa invertendo la disposizione
 # righe colonne e ottenendo una tabella con 2 colonne metrics e il nome dei sistemi messi a confronto
 def stats_relevance_tab(df, row_index, tab_title="", scientific_notation=True, round_to=4):
+    """
+        Using the data frame received in input this function will extract the information inside the data frame
+        with the row index used to access directly to a specific row and extract the data relevant to the row,
+        finally it will organise the dat into a latex table.
+
+        Parameters:
+        - df (pandas data  frame): text to be processed and changed if needed.
+        - row_index (tuple): it's a tuple used to access to the data frame.
+        - tab_title (str): string used as title for the table.
+        - scientific_notation (bool): used to apply the function that will format the results in scientific notation.
+        - round_to (int): indicate the number of cipher to round.
+
+        Returns:
+        - latex_table (str): The table with the value in string form.
+
+        Author:
+        - Diego Miccoli (Kozen88) <d.miccoli13@studenti.uniba>
+    """
     # Verifica se l'indice passato è presente in df.index
     if row_index not in df.index:
         # Se l'indice non è presente, crea un nuovo indice scambiando le sottostringhe
@@ -69,6 +86,19 @@ def stats_relevance_tab(df, row_index, tab_title="", scientific_notation=True, r
 # che calcolano i test statistici e modifica il dataframe creandone uno nuovo, nel quale le colonne multi-indice
 # vengono eliminate in base a removal_idx optando per la rimozione delle colonne 'statistics' oppure di quelle 'pvalue'
 def remove_stats_from_df(dataf, removal_idx):
+    """
+         It will remove a columns index and its value from the original data frame.
+
+         Parameters:
+         - dataf (pandas data frame): text to be processed and changed if needed.
+         - removal_idx (str): it's the name of the column of the data frame that will be removed.
+
+         Returns:
+         -  new_dataf (pandas data frame): the new data frame after processing.
+
+         Author:
+         - Diego Miccoli (Kozen88) <d.miccoli13@studenti.uniba>
+     """
     # Copia il DataFrame originale
     new_dataf = dataf.copy()
 
@@ -83,6 +113,21 @@ def remove_stats_from_df(dataf, removal_idx):
 
 # funzione per la creazione di un indice di accesso al dataframe
 def set_access_index(sys1, sys2, metric, type_val='pvalue'):
+    """
+         Create an index to access by row to a data frame.
+
+         Parameters:
+         - sys1 (str): first algorithm name.
+         - sys2 (str): second algorithm name.
+         - metric (str): metric to be accessed.
+         - type_val (str): value of the metric.
+
+         Returns:
+         -  (row_index, column_index) (tuple): index to gain accesss to the data frame.
+
+         Author:
+         - Diego Miccoli (Kozen88) <d.miccoli13@studenti.uniba>
+     """
     # Costruisci la tupla per l'indice della riga
     row_index = ('({}, {})'.format(sys1, sys2),)
     # Costruisci la tupla per l'indice della colonna
@@ -97,7 +142,19 @@ def set_access_index(sys1, sys2, metric, type_val='pvalue'):
 # tipo systm_n con n incrementale intero da 1 in poi a seconda di quanti systemi vengono messi
 # a confronto questo permetterà in seguito l'accesso al dataframe modificato con questi indici
 def change_system_name(df, sys_name):
-    # Funzione per sostituire gli indici con i valori del dizionario
+    """
+         This function using a dictionary will change the value of the data frame with the value of the keys.
+
+         Parameters:
+         - df (pandas data frame): data frame to be processed.
+         - sys_name (str): dictionary used for substitution.
+
+         Returns:
+         - df (pandas data frame) : data frame with the values changed.
+
+         Author:
+         - Diego Miccoli (Kozen88) <d.miccoli13@studenti.uniba>
+     """
     def replace_system_names(index_str):
         # Estrai i token dalla stringa dell'indice
         tokens = index_str.strip("()").replace("'", "").split(', ')
@@ -193,35 +250,52 @@ def from_dataframe_to_latex_table(df, col, title=""):
 
 #  nuova versione da testare terza implementazione
 def from_dataframe_to_latex_table(df, col, title=""):
-    # Verifica se il DataFrame è vuoto
+    """
+       Using the data frame received in input this function will organise the data a series of latex tables,
+       in particular the number of tables produced will depend on the number of columns present in the dataframe.
+
+       Parameters:
+       - df (pandas data  frame): text to be processed and changed if needed.
+       - col (int): number of columns to be showed for each table created.
+       - title (str): string used as title for the table. example of dict = {
+                                                            'system_1': 'CentroidVector',
+                                                            'system_2': 'IndexQuery',
+                                                            'system_3': 'Amar_single_source'
+                                                            }
+       Returns:
+       - latex_table (str): The table with the value in string form.
+
+       Author:
+       - Diego Miccoli (Kozen88) <d.miccoli13@studenti.uniba>
+   """
+    # test DataFrame
     if df.empty:
         return ""
 
-    # Assicurati che il numero di colonne sia minore o uguale a 3
+    # control check for structure of the table
     col = min(col, 3)
 
-    # Estrai le colonne del primo livello del header
+    # Extraction of the columns
     header_cols = df.columns.get_level_values(0).unique().tolist()
 
-    # Inizializza la stringa LaTeX vuota
     latex_str = ""
 
-    # Itera sulle colonne richieste
+    # Iteration on the columns of the dataframe in order to produce the tables needed
     for i in range(0, len(header_cols), col):
-        # Seleziona un sottoinsieme di colonne
+        # Selection of the columns to be used for the current table
         selected_cols = header_cols[i:i + col]
 
-        # Filtra il DataFrame per le colonne selezionate
+        # Filtering data frame with the selected columns
         df_selected = df[selected_cols]
 
-        # Aggiungi un sottotitolo alle tabelle (basato sulle colonne selezionate)
+        # Add subtitle based on the columns names used
         subtitle = ', '.join(selected_cols)
         table_title = f"{title} - {subtitle}"
 
-        # Genera la tabella LaTeX come stringa
+        # crreate the table with the values
         table_latex = df_selected.to_latex()
 
-        # Aggiungi un'istruzione LaTeX per una nuova tabella indipendente con il titolo
+        # Structuring of the table
         latex_str += f"\\begin{{table}}[H]\n\\centering\n"
         latex_str += "\\resizebox{\\columnwidth}{!}{%\n"
         latex_str += table_latex
